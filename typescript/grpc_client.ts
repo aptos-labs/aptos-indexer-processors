e9438f69b0f24cbf0be15c8fde105004e638bb30
rtso <renee@aptoslabs.com>
2023-04-20 15:19:19 -0400

    Open grpc stream

--- typescript/grpc_client.ts
+++ typescript/grpc_client.ts
@@ -1,8 +1,11 @@
-import { ArgumentParser } from "argparse";
-import { Config } from "./config";
-import * as messages from "./aptos/datastream/v1/datastream_pb";
 import * as grpc from "@grpc/grpc-js";
+import { ArgumentParser } from "argparse";
+import { Base64 } from "js-base64";
 import * as services from "./aptos/datastream/v1/datastream_grpc_pb";
+import * as datastreamMessages from "./aptos/datastream/v1/datastream_pb";
+import * as transactionMessages from "./aptos/transaction/testing1/v1/transaction_pb";
+import { Config } from "./config";
+import { parse } from "./grpc_parser";
 
 // Parse the config file
 const parser = new ArgumentParser({
@@ -21,22 +24,61 @@ const client = new services.IndexerStreamClient(
   grpc.credentials.createInsecure()
 );
 
-const request = new messages.RawDatastreamRequest();
+const request = new datastreamMessages.RawDatastreamRequest();
 request.setStartingVersion(config.starting_version);
 const metadata = new grpc.Metadata();
 metadata.set("x-aptos-data-authorization", config.indexer_api_key);
 
 // Create and start the streaming RPC
+const currentTransactionVersion = config.starting_version;
 const stream = client.rawDatastream(request, metadata);
-stream.on("data", function (response) {
-  console.log("got response");
-});
-stream.on("end", function () {
-  // The server has finished sending
-});
+stream.on(
+  "data",
+  function (response: datastreamMessages.RawDatastreamResponse) {
+    // Validate response chain ID matches expected chain ID
+    if (response.getChainId() != config.chain_id) {
+      throw new Error(
+        "Chain ID mismatch. Expected " +
+          config.chain_id +
+          " but got " +
+          response.getChainId()
+      );
+    }
+
+    const transactionsOutput = response.getData();
+
+    if (transactionsOutput == null) {
+      return;
+    }
+
+    for (const transactionOutput of transactionsOutput.getTransactionsList()) {
+      // Decode transaction object
+      const serializedTransactionString = Base64.decode(
+        transactionOutput.getEncodedProtoData_asB64()
+      );
+      const serializedTransactionBytes = Base64.toUint8Array(
+        serializedTransactionString
+      );
+      const transaction = transactionMessages.Transaction.deserializeBinary(
+        serializedTransactionBytes
+      );
+
+      // Validate transaction version is correct
+      if (transaction.getVersion() != currentTransactionVersion) {
+        throw new Error(
+          "Transaction version mismatch. Expected " +
+            currentTransactionVersion +
+            " but got " +
+            transaction.getVersion()
+        );
+      }
+
+      parse(transaction);
+    }
+  }
+);
+
 stream.on("error", function (e) {
   // An error has occurred and the stream has been closed.
-});
-stream.on("status", function (status) {
-  // process status
+  throw e;
 });
---
+++ typescript/grpc_parser.ts
@@ -0,0 +1,5 @@
+import { Transaction } from "./aptos/transaction/testing1/v1/transaction_pb";
+
+export function parse(transaction: Transaction) {
+  return [];
+}
