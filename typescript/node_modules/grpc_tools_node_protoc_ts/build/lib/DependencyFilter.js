7fe637e5a701e10168a2ad6a6e1a61ff582bd744
rtso <renee@aptoslabs.com>
2023-04-21 13:54:40 -0400

    Ignore node_modules

--- typescript/node_modules/grpc_tools_node_protoc_ts/build/lib/DependencyFilter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DependencyFilter = void 0;
-exports.DependencyFilter = [
-    "google/api/annotations.proto",
-];
-//# sourceMappingURL=DependencyFilter.js.map
