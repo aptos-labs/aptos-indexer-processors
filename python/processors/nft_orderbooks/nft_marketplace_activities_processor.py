ba8e02d70dff3e243a212f54d1f79c4b8dae0718
rtso <8248583+rtso@users.noreply.github.com>
2023-05-10 16:00:10 -0400

    Fix topaz parsing

--- python/processors/nft_orderbooks/nft_marketplace_activities_processor.py
+++ python/processors/nft_orderbooks/nft_marketplace_activities_processor.py
@@ -1,6 +1,14 @@
+import argparse
+import json
+import grpc
+
 from google.cloud.bigquery_storage_v1beta2 import BigQueryWriteClient
 from models.proto_autogen import nft_marketplace_activities_pb2
 from db_adapters.bigquery_stream_manager import BigqueryWriteManager
+from aptos.indexer.v1 import raw_data_pb2_grpc
+from aptos.indexer.v1 import raw_data_pb2
+from utils.config import Config
+from processors.nft_orderbooks import nft_orderbooks_parser
 
 project_id = "rtso-playground"
 dataset = "custom_processor"
@@ -9,38 +17,137 @@ write_stream = (
     "projects/{project_id}/datasets/{dataset}/tables/{table}/streams/_default"
 )
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--config", help="Path to config file", required=True)
+args = parser.parse_args()
+config = Config.from_yaml_file(args.config)
 
-def sample_append_rows():
-    # Create a write manager
-    write_manager = BigqueryWriteManager(
-        project_id=project_id,
-        dataset_id=dataset,
-        table_id=table,
-        pb2_descriptor=nft_marketplace_activities_pb2.NFTMarketplaceActivityRow.DESCRIPTOR,
-    )
+metadata = (("x-aptos-data-authorization", config.indexer_api_key),)
+options = [("grpc.max_receive_message_length", -1)]
+bq_write_manager = BigqueryWriteManager(
+    project_id=project_id,
+    dataset_id=dataset,
+    table_id=table,
+    pb2_descriptor=nft_marketplace_activities_pb2.NFTMarketplaceActivityRow.DESCRIPTOR,
+)
 
-    sample_data = []
-    row = nft_marketplace_activities_pb2.NFTMarketplaceActivityRow(
-        transaction_version=1,
-        event_index=0,
-        event_type="test",
-        standard_event_type="test",
-        creator_address="test",
-        collection="test",
-        token_name="test",
-        token_data_id="test",
-        collection_id="test",
-        price=1,
-        amount=1,
-        buyer="test",
-        seller="test",
-        json_data='{"test": "test"}',
-        marketplace="test",
-        contract_address="test",
-        entry_function_id_str="test",
-        transaction_timestamp=123,
-        inserted_at=123,
-        _CHANGE_TYPE="DELETE",
+starting_version = 0
+if config.starting_version != None:
+    # Start from config's starting version if set
+    starting_version = config.starting_version
+
+print(
+    json.dumps(
+        {
+            "message": "Connected to the indexer grpc",
+            "starting_version": starting_version,
+        }
     )
-    sample_data.append(row)
-    write_manager.write_rows(sample_data)
+)
+
+# Connect to grpc
+with grpc.insecure_channel(config.indexer_endpoint, options=options) as channel:
+    stub = raw_data_pb2_grpc.RawDataStub(channel)
+    current_transaction_version = starting_version
+
+    for response in stub.GetTransactions(
+        raw_data_pb2.GetTransactionsRequest(starting_version=starting_version),
+        metadata=metadata,
+    ):
+        chain_id = response.chain_id
+
+        if chain_id != config.chain_id:
+            raise Exception(
+                "Chain ID mismatch. Expected chain ID is: "
+                + str(config.chain_id)
+                + ", but received chain ID is: "
+                + str(chain_id)
+            )
+        print(
+            json.dumps(
+                {
+                    "message": "Response received",
+                    "starting_version": response.transactions[0].version,
+                }
+            )
+        )
+        transactions_output = response
+        for transaction in transactions_output.transactions:
+            transaction_version = transaction.version
+            if transaction_version != current_transaction_version:
+                raise Exception(
+                    "Transaction version mismatch. Expected transaction version is: "
+                    + str(current_transaction_version)
+                    + ", but received transaction version is: "
+                    + str(transaction_version)
+                )
+
+            try:
+                parsed_objs = nft_orderbooks_parser.parse(transaction)
+            except:
+                raise Exception(
+                    "Parser failed on transaction version " + str(transaction_version)
+                )
+
+            # with Session(engine) as session, session.begin():
+            #     # Insert Events into database
+            #     if parsed_objs is not None:
+            #         session.add_all(parsed_objs)
+
+            #     # Update latest processed version
+            #     session.merge(
+            #         NextVersionToProcess(
+            #             indexer_name=INDEXER_NAME,
+            #             next_version=current_transaction_version + 1,
+            #         )
+            #     )
+
+            # print(parsed_objs)
+
+            if (current_transaction_version % 1000) == 0:
+                print(
+                    json.dumps(
+                        {
+                            "message": "Successfully processed transaction",
+                            "last_success_transaction_version": current_transaction_version,
+                        }
+                    )
+                )
+
+            current_transaction_version += 1
+
+
+# def sample_append_rows():
+#     # Create a write manager
+#     write_manager = BigqueryWriteManager(
+#         project_id=project_id,
+#         dataset_id=dataset,
+#         table_id=table,
+#         pb2_descriptor=nft_marketplace_activities_pb2.NFTMarketplaceActivityRow.DESCRIPTOR,
+#     )
+
+#     sample_data = []
+#     row = nft_marketplace_activities_pb2.NFTMarketplaceActivityRow(
+#         transaction_version=1,
+#         event_index=0,
+#         event_type="test",
+#         standard_event_type="test",
+#         creator_address="test",
+#         collection="test",
+#         token_name="test",
+#         token_data_id="test",
+#         collection_id="test",
+#         price=1,
+#         amount=1,
+#         buyer="test",
+#         seller="test",
+#         json_data='{"test": "test"}',
+#         marketplace="test",
+#         contract_address="test",
+#         entry_function_id_str="test",
+#         transaction_timestamp=123,
+#         inserted_at=123,
+#         _CHANGE_TYPE="DELETE",
+#     )
+#     sample_data.append(row)
+#     write_manager.write_rows(sample_data)
---
+++ python/processors/nft_orderbooks/nft_orderbooks_parser_utils.py
@@ -0,0 +1,107 @@
+import re
+
+from aptos.transaction.testing1.v1 import transaction_pb2
+from enum import Enum
+from dataclasses import dataclass
+from google.protobuf import timestamp_pb2 as _timestamp_pb2
+from typing import List
+
+
+class MarketplaceName(Enum):
+    TOPAZ = "topaz"
+    SOUFFLE = "souffle"
+    BLUEMOVE = "bluemove"
+    OKX = "okx"
+    OZOZOZ = "ozozoz"
+    ITSRARE = "itsrare"
+    APTOMINGOS_AUCTION = "aptomingos_auction"
+
+
+class StandardMarketplaceEventType(Enum):
+    BID_CANCEL = "bid cancel"
+    BID_FILLED = "bid filled"
+    BID_PLACE = "bid place"
+    LISTING_CANCEL = "listing cancel"
+    LISTING_CHANGE = "listing change"
+    LISTING_FILLED = "listing_filled"
+    LISTING_PLACE = "listing_place"
+    UNKNOWN = "unknown"
+
+
+MARKETPLACE_ADDRESS_MATCH_REGEX_STRINGS = {
+    MarketplaceName.TOPAZ: "^0x2c7bccf7b31baf770fdbcc768d9e9cb3d87805e255355df5db32ac9a669010a2.*$",
+    MarketplaceName.SOUFFLE: "^0xf6994988bd40261af9431cd6dd3fcf765569719e66322c7a05cc78a89cd366d4.*FixedPriceMarket.*$",
+    MarketplaceName.BLUEMOVE: "^0xd1fd99c1944b84d1670a2536417e997864ad12303d19eac725891691b04d614e.*$",
+    MarketplaceName.OKX: "^0x1e6009ce9d288f3d5031c06ca0b19a334214ead798a0cb38808485bd6d997a43.*$",
+    MarketplaceName.OZOZOZ: "^0xded0c1249b522cecb11276d2fad03e6635507438fef042abeea3097846090bcd.*OzozozMarketplace.*$",
+    MarketplaceName.ITSRARE: "^0x143f6a7a07c76eae1fc9ea030dbd9be3c2d46e538c7ad61fe64529218ff44bc4.*$",
+    MarketplaceName.APTOMINGOS_AUCTION: "^0x98937acca8bc2c164dff158156ab06f9c99bbbb050129d7a514a37ccb1b8e49e.*$",
+}
+
+
+@dataclass
+class RawMarketplaceEvent:
+    transaction_version: int
+    event_index: int
+    event_type: str
+    json_data: str
+    contract_address: str
+    entry_function_name: str
+    transaction_timestamp: _timestamp_pb2.Timestamp
+
+
+def get_marketplace_events(
+    transaction: transaction_pb2.Transaction, marketplaceName: MarketplaceName
+) -> List[RawMarketplaceEvent]:
+    # Filter out all non-user transactions
+    if transaction.type != transaction_pb2.Transaction.TRANSACTION_TYPE_USER:
+        return []
+
+    transaction_version = transaction.version
+    transaction_timestamp = transaction.timestamp
+    transaction_timestamp_pb = _timestamp_pb2.Timestamp()
+    transaction_timestamp_pb.seconds = transaction_timestamp.seconds
+    transaction_timestamp_pb.nanos = transaction_timestamp.nanos
+
+    user_transaction = transaction.user
+    user_transaction_request = user_transaction.request
+    transaction_payload = user_transaction_request.payload
+    entry_function_payload = transaction_payload.entry_function_payload
+    entry_function_id_str = entry_function_payload.entry_function_id_str
+
+    contract_addr_match = re.search("([^:]*)::.*?", entry_function_id_str)
+    if contract_addr_match == None:
+        contract_addr = ""
+    else:
+        contract_addr = contract_addr_match.group(1)
+
+    entry_function_name_match = re.search(
+        "[^:]*?::([^:]*::[^:]*)", entry_function_id_str
+    )
+    if entry_function_name_match == None:
+        entry_function_name = ""
+    else:
+        entry_function_name = entry_function_name_match.group(1)
+
+    raw_marketplace_events = []
+    for event_index, event in enumerate(user_transaction.events):
+        event_type = event.type_str
+        event_type_match = re.search(
+            MARKETPLACE_ADDRESS_MATCH_REGEX_STRINGS[marketplaceName], event_type
+        )
+        if event_type_match == None:
+            continue
+
+        raw_marketplace_event = RawMarketplaceEvent(
+            transaction_version,
+            event_index,
+            event_type,
+            json_data=event.data,
+            contract_address=contract_addr,
+            entry_function_name=entry_function_name,
+            transaction_timestamp=transaction_timestamp_pb,
+        )
+
+        raw_marketplace_events.append(raw_marketplace_event)
+
+    return raw_marketplace_events
