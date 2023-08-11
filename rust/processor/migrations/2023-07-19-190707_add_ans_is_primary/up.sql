c998e9afec71db28606ac23fad7a7a7465b55caf
rtso <8248583+rtso@users.noreply.github.com>
2023-08-11 13:05:26 -0700

    Comments

--- rust/processor/migrations/2023-07-19-190707_add_ans_is_primary/up.sql
+++ rust/processor/migrations/2023-07-19-190707_add_ans_is_primary/up.sql
@@ -1,18 +1,17 @@
 ALTER TABLE current_ans_lookup
-ADD COLUMN is_primary BOOLEAN NOT NULL DEFAULT FALSE,
-ADD COLUMN is_deleted BOOLEAN NOT NULL DEFAULT FALSE;
+ADD COLUMN IF NOT EXISTS is_primary BOOLEAN NOT NULL DEFAULT FALSE,
+ADD COLUMN IF NOT EXISTS is_deleted BOOLEAN NOT NULL DEFAULT FALSE;
 
-CREATE TABLE ans_lookup (
+CREATE TABLE IF NOT EXISTS ans_lookup (
     transaction_version BIGINT NOT NULL,
     domain VARCHAR(64) NOT NULL,
     -- if subdomain is null set to empty string
     subdomain VARCHAR(64) NOT NULL,
     registered_address VARCHAR(66),
     expiration_timestamp TIMESTAMP,
-    token_name VARCHAR(128) NOT NULL,
+    token_name VARCHAR(140) NOT NULL,
     is_primary BOOLEAN,
-    is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
-    transaction_timestamp TIMESTAMP NOT NULL,
+    is_deleted BOOLEAN,
     inserted_at TIMESTAMP NOT NULL DEFAULT NOW(),
     -- Constraints 
     PRIMARY KEY (transaction_version, domain, subdomain)
