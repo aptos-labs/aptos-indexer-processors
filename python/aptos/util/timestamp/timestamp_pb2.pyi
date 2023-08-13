624b1d0ee9a4df804b060a90954b6f630c39c351
xbtmatt <xbtchunky@gmail.com>
2023-08-12 18:02:30 -0700

    Adding generated python and typescript files by removing them from .gitignore. added __init__.py to `aptos` and `utils` folders
    __init__.py

---
+++ python/aptos/util/timestamp/timestamp_pb2.pyi
@@ -0,0 +1,13 @@
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class Timestamp(_message.Message):
+    __slots__ = ["seconds", "nanos"]
+    SECONDS_FIELD_NUMBER: _ClassVar[int]
+    NANOS_FIELD_NUMBER: _ClassVar[int]
+    seconds: int
+    nanos: int
+    def __init__(self, seconds: _Optional[int] = ..., nanos: _Optional[int] = ...) -> None: ...
