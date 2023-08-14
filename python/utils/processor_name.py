7e3603e3162a6dc7f4385ce4a8dde31e94dc75a8
xbtmatt <xbtchunky@gmail.com>
2023-08-13 23:50:21 -0700

    Adding coin-flip module for tutorial, in process, will finish later

--- python/utils/processor_name.py
+++ python/utils/processor_name.py
@@ -5,3 +5,4 @@ class ProcessorName(Enum):
     EXAMPLE_EVENT_PROCESSOR = "python_example_event_processor"
     NFT_MARKETPLACE_V1_PROCESSOR = "nft_marketplace_v1_processor"
     NFT_MARKETPLACE_V2_PROCESSOR = "nft_marketplace_v2_processor"
+    COIN_FLIP = "coin_flip"
--- python/utils/worker.py
+++ python/utils/worker.py
@@ -25,6 +25,7 @@ from utils.processor_name import ProcessorName
 from processors.example_event_processor.processor import ExampleEventProcessor
 from processors.nft_orderbooks.nft_marketplace_processor import NFTMarketplaceProcesser
 from processors.nft_marketplace_v2.processor import NFTMarketplaceV2Processor
+from processors.coin_flip.processor import CoinFlipProcessor
 
 INDEXER_GRPC_BLOB_STORAGE_SIZE = 1000
 
@@ -46,6 +47,8 @@ class IndexerProcessorServer:
                 self.processor = NFTMarketplaceProcesser()
             case ProcessorName.NFT_MARKETPLACE_V2_PROCESSOR.value:
                 self.processor = NFTMarketplaceV2Processor()
+            case ProcessorName.COIN_FLIP.value:
+                self.processor = CoinFlipProcessor()
             case _:
                 raise Exception(
                     "Invalid processor name"
