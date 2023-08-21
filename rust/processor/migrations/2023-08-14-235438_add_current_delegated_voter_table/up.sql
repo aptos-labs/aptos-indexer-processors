2f3434a5379004193047931ccbf29ac0f11ab664
bowenyang007 <carlyang007@gmail.com>
2023-08-21 10:54:07 -0700

    change migration

--- rust/processor/migrations/2023-08-14-235438_add_current_delegated_voter_table/up.sql
+++ rust/processor/migrations/2023-08-14-235438_add_current_delegated_voter_table/up.sql
@@ -8,10 +8,10 @@ CREATE TABLE IF NOT EXISTS current_delegated_voter (
   pending_voter VARCHAR(66),
   last_transaction_version BIGINT NOT NULL,
   last_transaction_timestamp TIMESTAMP NOT NULL,
+  inserted_at TIMESTAMP NOT NULL DEFAULT NOW(),
   PRIMARY KEY (delegation_pool_address, delegator_address)
 );
-CREATE INDEX IF NOT EXISTS cdv_dpa_idx ON current_delegated_voter (delegation_pool_address);
 CREATE INDEX IF NOT EXISTS cdv_da_index ON current_delegated_voter (delegator_address);
-CREATE INDEX IF NOT EXISTS cdv_th_index ON current_delegated_voter (table_handle);
 CREATE INDEX IF NOT EXISTS cdv_v_index ON current_delegated_voter (voter);
 CREATE INDEX IF NOT EXISTS cdv_pv_index ON current_delegated_voter (pending_voter);
+CREATE INDEX IF NOT EXISTS cdv_insat_index ON current_delegated_voter (inserted_at);
