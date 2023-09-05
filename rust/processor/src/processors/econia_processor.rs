1c0b279ddb2df266bada235b772985a7c61ddbee
Elliott Dehnbostel <elliott@econialabs.com>
2023-09-05 12:37:38 -0400

    insertion into db is a go

--- rust/processor/src/processors/econia_processor.rs
+++ rust/processor/src/processors/econia_processor.rs
@@ -20,19 +20,28 @@ use crate::{
 use anyhow::bail;
 use aptos_indexer_protos::transaction::v1::{write_set_change::Change, Transaction};
 use async_trait::async_trait;
-use diesel::{pg::upsert::excluded, result::Error, ExpressionMethods, PgConnection};
+use chrono::{NaiveDateTime, DateTime, Utc};
+use diesel::{pg::upsert::excluded, result::Error, ExpressionMethods, PgConnection, RunQueryDsl};
 use field_count::FieldCount;
 use std::{collections::HashMap, fmt::Debug};
 use tracing::error;
+use serde_json::Value;
+use std::str::FromStr;
+use bigdecimal::BigDecimal;
+use econia_db::schema::market_registration_events;
+use econia_db::models::NewMarketRegistrationEvent;
+// use bigdecimal::BigDecimal;
+// use std::str::FromStr;
 
 pub const NAME: &str = "econia_processor";
 pub struct EconiaTransactionProcessor {
     connection_pool: PgDbPool,
+    econia_address: String,
 }
 
 impl EconiaTransactionProcessor {
-    pub fn new(connection_pool: PgDbPool) -> Self {
-        Self { connection_pool }
+    pub fn new(connection_pool: PgDbPool, econia_address: String) -> Self {
+        Self { connection_pool, econia_address }
     }
 }
 
@@ -45,6 +54,45 @@ impl Debug for EconiaTransactionProcessor {
             state.connections, state.idle_connections
         )
     }
+}
+
+struct TypeInfo {
+    address: String,
+    module_name: String,
+    struct_name: String,
+}
+
+fn hex_to_string(hex: &str) -> String {
+    let hex_no_prefix = &hex[2..];
+    let hex_bytes = hex::decode(hex_no_prefix).unwrap();
+    String::from_utf8(hex_bytes).unwrap()
+}
+
+fn opt_value_to_big_decimal(value: Option<&Value>) -> BigDecimal {
+    let lot_size_str = value
+        .and_then(Value::as_str)
+        .expect("key not found or not a string");
+    BigDecimal::from_str(lot_size_str).expect("Failed to parse BigDecimal")
+}
+
+fn value_is_not_empty_string(val: &Value) -> bool {
+    val.as_str()
+        .expect("Value is not a string")
+        .is_empty() == false
+}
+
+fn insert_market_registration_events(
+    conn: &mut PgConnection,
+    events: Vec<NewMarketRegistrationEvent>,
+) -> Result<(), diesel::result::Error> {
+    execute_with_better_error(
+        conn,
+        diesel::insert_into(market_registration_events::table)
+            .values(&events)
+            .on_conflict_do_nothing(),
+        None,
+    )?;
+    Ok(())
 }
 
 #[async_trait]
@@ -63,6 +111,92 @@ impl ProcessorTrait for EconiaTransactionProcessor {
         let mut conn = self.get_conn();
         let (txns, txn_details, events, write_set_changes, wsc_details) =
             TransactionModel::from_transactions(&transactions);
+
+        // create a hashmap to store block_height to timestamp
+        let mut block_height_to_timestamp: HashMap<i64, DateTime<Utc>> = HashMap::new();
+        
+        // iterate through the transactions and populate the map
+        for txn_detail in txn_details {
+            match txn_detail {
+                TransactionDetail::User(user_transaction, _) => {
+                    block_height_to_timestamp.insert(
+                        user_transaction.block_height,
+                        DateTime::from_utc(user_transaction.timestamp, Utc)
+                    );
+                }
+                TransactionDetail::BlockMetadata(block_metadata_transaction) => {
+                    block_height_to_timestamp.insert(
+                        block_metadata_transaction.block_height,
+                        DateTime::from_utc(block_metadata_transaction.timestamp, Utc)
+                    );
+                }
+            }
+        }
+
+        let market_registration_type = format!("{}::registry::MarketRegistrationEvent", self.econia_address);
+        let mut market_registration_events = vec![];
+        for event in events {
+            if event.type_ == market_registration_type {
+                let market_id = opt_value_to_big_decimal(event.data.get("market_id"));
+                let lot_size = opt_value_to_big_decimal(event.data.get("lot_size"));
+                let tick_size = opt_value_to_big_decimal(event.data.get("tick_size"));
+                let min_size = opt_value_to_big_decimal(event.data.get("min_size"));
+                let underwriter_id = opt_value_to_big_decimal(event.data.get("underwriter_id"));
+                let time = *block_height_to_timestamp.get(&event.transaction_block_height).unwrap();
+                let base_name_generic;
+                let base_account_address;
+                let base_module_name;
+                let base_struct_name;
+                if value_is_not_empty_string(&event.data["base_name_generic"]) {
+                    base_name_generic = Some(String::from(event.data["base_name_generic"].as_str().unwrap()));
+                    base_account_address = None;
+                    base_module_name = None;
+                    base_struct_name = None;
+                } else {
+                    base_name_generic = None;
+                    base_account_address = Some(String::from(event.data["base_type"]["account_address"].as_str().unwrap()));
+                    let base_module_name_hex = event.data["base_type"]["module_name"].as_str().unwrap();
+                    let base_struct_name_hex = event.data["base_type"]["struct_name"].as_str().unwrap();
+                    base_module_name = Some(hex_to_string(base_module_name_hex));
+                    base_struct_name = Some(hex_to_string(base_struct_name_hex));
+                }
+                let quote_account_address = String::from(event.data["quote_type"]["account_address"].as_str().unwrap());
+                let quote_module_name_hex = event.data["quote_type"]["module_name"].as_str().unwrap();
+                let quote_struct_name_hex = event.data["quote_type"]["struct_name"].as_str().unwrap();
+                let quote_module_name = hex_to_string(quote_module_name_hex);
+                let quote_struct_name = hex_to_string(quote_struct_name_hex);
+
+                let market_registration_event = NewMarketRegistrationEvent {
+                    market_id,
+                    time,
+                    base_name_generic,
+                    base_account_address,
+                    base_module_name,
+                    base_struct_name,
+                    quote_account_address,
+                    quote_module_name,
+                    quote_struct_name,
+                    lot_size,
+                    tick_size,
+                    min_size,
+                    underwriter_id,
+                };
+                market_registration_events.push(market_registration_event);
+            }
+        }
+
+        conn
+            .build_transaction()
+            .read_write()
+            .run::<_, Error, _>(|pg_conn| {
+                insert_market_registration_events(
+                    pg_conn,
+                    market_registration_events,
+                )
+            })
+            .expect("Expected a successful insertion into the table");
+
+        panic!("I reached the end of times!");
 
         let mut signatures = vec![];
         let mut user_transactions = vec![];
@@ -166,7 +300,7 @@ impl ProcessorTrait for EconiaTransactionProcessor {
             .sort_by(|a, b| (&a.table_handle, &a.key_hash).cmp(&(&b.table_handle, &b.key_hash)));
         table_metadata.sort_by(|a, b| a.handle.cmp(&b.handle));
         all_current_objects.sort_by(|a, b| a.object_address.cmp(&b.object_address));
-        
+
         Ok((start_version, end_version))
     }
 
--- rust/processor/src/worker.rs
+++ rust/processor/src/worker.rs
@@ -40,7 +40,7 @@ use tokio::sync::mpsc::error::TryRecvError;
 use tonic::Streaming;
 use tracing::{error, info};
 
-pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("./../../../../dbv2/migrations");
 /// GRPC request metadata key for the token ID.
 const GRPC_API_GATEWAY_API_KEY_HEADER: &str = "authorization";
 /// GRPC request metadata key for the request name. This is used to identify the
@@ -74,6 +74,7 @@ pub struct Worker {
     pub nft_points_contract: Option<String>,
     pub pubsub_topic_name: Option<String>,
     pub google_application_credentials: Option<String>,
+    pub econia_address: Option<String>,
 }
 
 impl Worker {
@@ -123,6 +124,7 @@ impl Worker {
             nft_points_contract,
             pubsub_topic_name,
             google_application_credentials,
+            econia_address,
         }
     }
 
@@ -215,7 +217,14 @@ impl Worker {
                 self.ans_v1_name_records_table_handle.clone(),
             )),
             Processor::EconiaProcessor => {
-                Arc::new(EconiaTransactionProcessor::new(self.db_pool.clone()))
+                Arc::new(
+                    EconiaTransactionProcessor::new(
+                        self.db_pool.clone(),
+                        self.econia_address
+                            .clone()
+                            .expect("Econia processor requires an exchange address")
+                    )
+                )
             },
         };
         let processor_name = processor.name();
