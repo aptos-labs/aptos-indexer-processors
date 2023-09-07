5ceafae58bb258916ab914d698671b29f54984cc
rtso <8248583+rtso@users.noreply.github.com>
2023-09-07 13:21:40 -0400

    Add current_aptos_names_v2

--- rust/processor/migrations/2023-09-06-183718_add_ans_v2/up.sql
+++ rust/processor/migrations/2023-09-06-183718_add_ans_v2/up.sql
@@ -13,6 +13,7 @@ CREATE TABLE IF NOT EXISTS current_ans_lookup_v2 (
     -- Constraints 
     PRIMARY KEY (domain, subdomain)
 );
+
 CREATE TABLE IF NOT EXISTS ans_lookup_v2 (
     transaction_version BIGINT NOT NULL,
     write_set_change_index BIGINT NOT NULL,
@@ -30,3 +31,48 @@ CREATE TABLE IF NOT EXISTS ans_lookup_v2 (
 );
 
 -- TODO: Create view for current_aptos_names
+DROP VIEW IF EXISTS current_aptos_names;
+CREATE OR REPLACE VIEW current_aptos_names AS 
+WITH current_aptos_names_v1 AS (
+    SELECT
+        current_ans_lookup.domain,
+        current_ans_lookup.subdomain,
+        current_ans_lookup.token_name,
+        current_ans_lookup.registered_address,
+        current_ans_lookup.expiration_timestamp,
+        greatest(
+            current_ans_lookup.last_transaction_version,
+            current_ans_primary_name.last_transaction_version
+        ) AS last_transaction_version,
+        coalesce(NOT current_ans_primary_name.is_deleted, false) AS is_primary
+    FROM
+        current_ans_lookup
+        LEFT JOIN current_ans_primary_name ON current_ans_lookup.token_name = current_ans_primary_name.token_name
+    WHERE
+        current_ans_lookup.expiration_timestamp > CURRENT_TIMESTAMP
+        AND current_ans_lookup.is_deleted IS false
+),
+current_aptos_names_v2 AS (
+    SELECT
+        domain,
+        subdomain,
+        token_name,
+        registered_address,
+        expiration_timestamp,
+        last_transaction_version,
+        is_primary
+    FROM
+        current_ans_lookup_v2
+    WHERE
+        expiration_timestamp > CURRENT_TIMESTAMP
+        AND is_deleted IS false
+)
+SELECT
+    *
+FROM
+    current_aptos_names_v2
+UNION
+SELECT
+    *
+FROM
+    current_aptos_names_v1;
