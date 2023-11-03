2d5cb211a89a8705674e9e1e741c841dd899c558
bowenyang007 <carlyang007@gmail.com>
2023-11-02 21:03:51 -0700

    [rust] Index events type, backfill fungible assets fix, and create spam table (#171)
    * add a new field for indexed event type and spam table
    
    * rebase and fix
    
    * lint
    
    * enable event backfilling
    
    * fix type in hasura metadata

--- rust/processor/src/models/events_models/events.rs
+++ rust/processor/src/models/events_models/events.rs
@@ -2,10 +2,16 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #![allow(clippy::extra_unused_lifetimes)]
-use crate::{schema::events, utils::util::standardize_address};
+use crate::{
+    schema::events,
+    utils::util::{standardize_address, truncate_str},
+};
 use aptos_protos::transaction::v1::Event as EventPB;
 use field_count::FieldCount;
 use serde::{Deserialize, Serialize};
+
+// p99 currently is 303 so using 300 as a safe max length
+const EVENT_TYPE_MAX_LENGTH: usize = 300;
 
 #[derive(Debug, Deserialize, FieldCount, Identifiable, Insertable, Serialize)]
 #[diesel(primary_key(transaction_version, event_index))]
@@ -19,6 +25,7 @@ pub struct Event {
     pub type_: String,
     pub data: serde_json::Value,
     pub event_index: i64,
+    pub indexed_type: String,
 }
 
 impl Event {
@@ -28,6 +35,7 @@ impl Event {
         transaction_block_height: i64,
         event_index: i64,
     ) -> Self {
+        let t: &str = event.type_str.as_ref();
         Event {
             account_address: standardize_address(
                 event.key.as_ref().unwrap().account_address.as_str(),
@@ -36,9 +44,10 @@ impl Event {
             sequence_number: event.sequence_number as i64,
             transaction_version,
             transaction_block_height,
-            type_: event.type_str.clone(),
+            type_: t.to_string(),
             data: serde_json::from_str(event.data.as_str()).unwrap(),
             event_index,
+            indexed_type: truncate_str(t, EVENT_TYPE_MAX_LENGTH),
         }
     }
 
