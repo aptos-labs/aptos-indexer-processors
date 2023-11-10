5f743c2cdb04b82888f239d90ae53c4d6462215b
larry-aptos <112209412+larry-aptos@users.noreply.github.com>
2023-11-09 16:14:40 -0800

    fix the metric to consolidate with rust ones. (#185)

--- python/utils/metrics.py
+++ python/utils/metrics.py
@@ -1,13 +1,13 @@
 from prometheus_client import Counter, Gauge
 
 PROCESSED_TRANSACTIONS_COUNTER = Counter(
-    "indexer_py_processor_processed_transactions",
+    "indexer_processor_processed_transactions",
     "Number of transactions processed",
     ["processor_name"],
 )
 
 LATEST_PROCESSED_VERSION = Gauge(
-    "indexer_py_processor_latest_processed_version",
+    "indexer_processor_latest_version",
     "Latest processed version",
     ["processor_name"],
 )
