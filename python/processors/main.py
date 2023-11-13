7dd547e59c88ea4882fd72ac84b3a5e2ad83b22f
Larry Liu <larry@aptoslabs.com>
2023-11-12 23:17:22 -0800

    add new dependency for json logging.

--- python/processors/main.py
+++ python/processors/main.py
@@ -1,8 +1,17 @@
 import argparse
+import logging
+from pythonjsonlogger import jsonlogger
 from utils.config import Config
 from utils.worker import IndexerProcessorServer
 
 if __name__ == "__main__":
+    # Setup logging with JSON formatter.
+    logger = logging.getLogger()
+    logHandler = logging.StreamHandler()
+    formatter = jsonlogger.JsonFormatter()
+    logHandler.setFormatter(formatter)
+    logger.addHandler(logHandler)
+
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--config", help="Path to config file", required=True)
     args = parser.parse_args()
