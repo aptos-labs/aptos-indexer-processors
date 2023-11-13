63e68e56570d4953f4661f3d31617be7ad7d35d6
Larry Liu <larry@aptoslabs.com>
2023-11-13 00:31:54 -0800

    add the logging util that matches with rust worker.

--- python/processors/main.py
+++ python/processors/main.py
@@ -3,14 +3,20 @@ import logging
 from pythonjsonlogger import jsonlogger
 from utils.config import Config
 from utils.worker import IndexerProcessorServer
+from utils.logging import JsonFormatter, CustomLogger
 
 if __name__ == "__main__":
-    # Setup logging with JSON formatter.
-    logger = logging.getLogger()
-    logHandler = logging.StreamHandler()
-    formatter = jsonlogger.JsonFormatter()
-    logHandler.setFormatter(formatter)
-    logger.addHandler(logHandler)
+   # Configure the logger
+    logger = CustomLogger('default_python_logger')
+    logger.setLevel(logging.DEBUG)
+
+    # Create a stream handler for stdout
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(JsonFormatter())
+
+    # Add the stream handler to the logger
+    logger.addHandler(stream_handler)
+    logging.root = logger
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--config", help="Path to config file", required=True)
