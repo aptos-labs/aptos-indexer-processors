19247f73ea1a1da8ba07cefa14f5bfda7c4177ca
Justin Chang <justin@aptoslabs.com>
2023-11-15 19:30:27 -0800

    some progress, runs fine now

--- rust/processor/src/models/aptos_tournament_models/aptos_tournament_utils.rs
+++ rust/processor/src/models/aptos_tournament_models/aptos_tournament_utils.rs
@@ -1,4 +1,7 @@
-use crate::models::default_models::move_resources::MoveResource;
+use crate::models::{
+    default_models::move_resources::MoveResource,
+    token_v2_models::v2_token_utils::ObjectWithMetadata,
+};
 use anyhow::Context;
 use aptos_protos::transaction::v1::{Event, WriteResource};
 use serde::{Deserialize, Serialize};
@@ -12,11 +15,12 @@ pub type AptosTournamentAggregatedDataMapping = HashMap<String, AptosTournamentA
 pub struct AptosTournamentAggregatedData {
     pub aptos_tournament: Option<AptosTournament>,
     pub current_round: Option<CurrentRound>,
-    pub refs: Option<Refs>,
+    // pub refs: Option<Refs>,
     pub rps_game: Option<RPSGame>,
     pub tournament_director: Option<TournamentDirector>,
     pub tournament_state: Option<TournamentState>,
     pub tournament_token: Option<TournamentToken>,
+    pub object: ObjectWithMetadata,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -26,6 +30,7 @@ pub struct RefVec {
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct RefSelf {
+    #[serde(rename = "self")]
     self_: String,
 }
 
@@ -86,44 +91,49 @@ impl AptosTournamentEvent {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct Refs {
-    burn_ref: RefVec,
-    delete_ref: RefSelf,
-    extend_ref: RefSelf,
-    property_mutator_ref: RefVec,
-    transfer_ref: RefSelf,
-}
+// #[derive(Serialize, Deserialize, Debug, Clone)]
+// pub struct BurnRef {
+//     vec: Vec<BurnRef2>,
+// }
 
-impl Refs {
-    pub fn from_write_resource(
-        addr: &str,
-        write_resource: &WriteResource,
-        txn_version: i64,
-    ) -> anyhow::Result<Option<Self>> {
-        let type_str = MoveResource::get_outer_type_from_resource(write_resource);
-        if !AptosTournamentResource::is_resource_supported(addr, type_str.as_str()) {
-            return Ok(None);
-        }
-        let resource = MoveResource::from_write_resource(
-            write_resource,
-            0, // Placeholder, this isn't used anyway
-            txn_version,
-            0, // Placeholder, this isn't used anyway
-        );
+// #[derive(Serialize, Deserialize, Debug, Clone)]
+// pub struct Refs {
+//     burn_ref: BurnRef,
+//     delete_ref: RefSelf,
+//     extend_ref: RefSelf,
+//     property_mutator_ref: RefVec,
+//     transfer_ref: RefSelf,
+// }
 
-        if let AptosTournamentResource::Refs(inner) = AptosTournamentResource::from_resource(
-            addr,
-            &type_str,
-            resource.data.as_ref().unwrap(),
-            txn_version,
-        )? {
-            Ok(Some(inner))
-        } else {
-            Ok(None)
-        }
-    }
-}
+// impl Refs {
+//     pub fn from_write_resource(
+//         addr: &str,
+//         write_resource: &WriteResource,
+//         txn_version: i64,
+//     ) -> anyhow::Result<Option<Self>> {
+//         let type_str = MoveResource::get_outer_type_from_resource(write_resource);
+//         if !AptosTournamentResource::is_resource_supported(addr, type_str.as_str()) {
+//             return Ok(None);
+//         }
+//         let resource = MoveResource::from_write_resource(
+//             write_resource,
+//             0, // Placeholder, this isn't used anyway
+//             txn_version,
+//             0, // Placeholder, this isn't used anyway
+//         );
+
+//         if let AptosTournamentResource::Refs(inner) = AptosTournamentResource::from_resource(
+//             addr,
+//             &type_str,
+//             resource.data.as_ref().unwrap(),
+//             txn_version,
+//         )? {
+//             Ok(Some(inner))
+//         } else {
+//             Ok(None)
+//         }
+//     }
+// }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct RPSGame {
@@ -202,9 +212,9 @@ impl CurrentRound {
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct TournamentDirector {
-    max_num_winners: u64,
-    max_players: u64,
-    players_joined: u64,
+    max_num_winners: String,
+    max_players: String,
+    players_joined: String,
     secondary_admin_address: RefVec,
     tournament_name: String,
 }
@@ -240,6 +250,7 @@ impl TournamentDirector {
         }
     }
 }
+
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct TournamentState {
     has_ended: bool,
@@ -280,7 +291,7 @@ impl TournamentState {
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct TournamentToken {
-    last_recorded_round: u64,
+    last_recorded_round: String,
     room_address: String,
     tournament_address: String,
 }
@@ -359,7 +370,7 @@ pub enum AptosTournamentResource {
     AptosTournament(AptosTournament),
     CurrentRound(CurrentRound),
     // MatchMaker(MatchMaker), // TODO: Figure out how to handle multiple game types
-    Refs(Refs),
+    // Refs(Refs), // TODO: Figure out how to flatten them or better index nested structs
     // Room(Room),       // TODO: Figure out how to handle multiple game types
     // Round(Round), // TODO: Figure out how to handle multiple game types
     RPSGame(RPSGame), // TODO: Figure out how to handle multiple game types
@@ -371,13 +382,13 @@ pub enum AptosTournamentResource {
 impl AptosTournamentResource {
     pub fn is_resource_supported(addr: &str, data_type: &str) -> bool {
         [
+            // format!("{}::object_refs::Refs", addr),
+            // format!("{}::matchmaker::MatchMaker", addr), // TODO: Figure out how to handle multiple game types
+            // format!("{}::round::Round", addr), // TODO: Figure out how to handle multiple game types
+            // format!("{}::room::Room", addr),   // TODO: Figure out how to handle multiple game types
             format!("{}::aptos_tournament::AptosTournament", addr),
             format!("{}::events::GameOverEvent", addr),
-            format!("{}::matchmaker::MatchMaker", addr), // TODO: Figure out how to handle multiple game types
-            format!("{}::object_refs::Refs", addr),
             format!("{}::rock_paper_scissor::Game", addr),
-            format!("{}::round::Round", addr), // TODO: Figure out how to handle multiple game types
-            format!("{}::room::Room", addr),   // TODO: Figure out how to handle multiple game types
             format!("{}::token_manager::TournamentToken", addr),
             format!("{}::tournament_manager::CurrentRound", addr),
             format!("{}::tournament_manager::TournamentDirector", addr),
@@ -396,9 +407,9 @@ impl AptosTournamentResource {
             x if x == format!("{}::aptos_tournament::AptosTournament", addr) => {
                 serde_json::from_value(data.clone()).map(|inner| Some(Self::AptosTournament(inner)))
             },
-            x if x == format!("{}::object_refs::Refs", addr) => {
-                serde_json::from_value(data.clone()).map(|inner| Some(Self::Refs(inner)))
-            },
+            // x if x == format!("{}::object_refs::Refs", addr) => {
+            //     serde_json::from_value(data.clone()).map(|inner| Some(Self::Refs(inner)))
+            // },
             x if x == format!("{}::rock_paper_scissor::Game", addr) => {
                 serde_json::from_value(data.clone()).map(|inner| Some(Self::RPSGame(inner)))
             },
