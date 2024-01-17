4336f690d2355a69e44295c3d3a69e59df31336b
Daniel Porteous <daniel@dport.me>
2024-01-17 19:42:05 +0000

    [Rust SDK] Add dispatcher

--- rust/sdk/src/lib.rs
+++ rust/sdk/src/lib.rs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 pub mod counters;
+pub mod dispatcher;
 pub mod processor;
 pub mod progress_storage;
 pub mod stream_subscriber;
--- rust/sdk/src/utils/mod.rs
+++ rust/sdk/src/utils/mod.rs
@@ -27,15 +27,25 @@ pub fn parse_timestamp_secs(ts: u64, version: i64) -> chrono::NaiveDateTime {
     .unwrap_or_else(|| panic!("Could not parse timestamp {:?} for version {}", ts, version))
 }
 
+/// Convert the protobuf timestamp to unixtime
+pub fn timestamp_to_unixtime(timestamp: &Timestamp) -> f64 {
+    timestamp.seconds as f64 + timestamp.nanos as f64 * 1e-9
+}
+
 /// Convert the protobuf timestamp to ISO format
 pub fn timestamp_to_iso(timestamp: &Timestamp) -> String {
     let dt = parse_timestamp(timestamp, 0);
     dt.format("%Y-%m-%dT%H:%M:%S%.9fZ").to_string()
 }
 
-/// Convert the protobuf timestamp to unixtime
-pub fn timestamp_to_unixtime(timestamp: &Timestamp) -> f64 {
-    timestamp.seconds as f64 + timestamp.nanos as f64 * 1e-9
+/// Convert the protobuf Timestamp to epcoh time in seconds.
+pub fn time_diff_since_pb_timestamp_in_secs(timestamp: &Timestamp) -> f64 {
+    let current_timestamp = std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .expect("SystemTime before UNIX EPOCH!")
+        .as_secs_f64();
+    let transaction_time = timestamp.seconds as f64 + timestamp.nanos as f64 * 1e-9;
+    current_timestamp - transaction_time
 }
 
 #[cfg(test)]
