4d134242fbf9fc17b0ee39c18971955d8eceb2b1
CapCap <max@aptoslabs.com>
2024-01-23 00:00:36 -0800

    log cpu num and thread count

--- rust/processor/src/main.rs
+++ rust/processor/src/main.rs
@@ -5,14 +5,17 @@ use anyhow::Result;
 use clap::Parser;
 use processor::IndexerGrpcProcessorConfig;
 use server_framework::ServerArgs;
+use tracing::info;
 
 const RUNTIME_WORKER_MULTIPLIER: usize = 2;
 
 fn main() -> Result<()> {
-    let worker_threads = (num_cpus::get() * RUNTIME_WORKER_MULTIPLIER).min(16);
-    println!(
-        "Starting processor tokio runtime with worker_threads: {}",
-        worker_threads
+    let num_cpus = num_cpus::get();
+    let worker_threads = (num_cpus * RUNTIME_WORKER_MULTIPLIER).min(16);
+    info!(
+        num_cpus = num_cpus,
+        num_worker_threads = worker_threads,
+        "[Processor] Starting processor tokio runtime",
     );
 
     tokio::runtime::Builder::new_multi_thread()
