6b097cbd9dbbecfde4572a18c39bdb07757e9318
Justin Chang <justin@aptoslabs.com>
2024-02-20 16:21:21 -0800

    add metric

--- rust/processor/src/utils/stream.rs
+++ rust/processor/src/utils/stream.rs
@@ -1,6 +1,9 @@
 // Copyright © Aptos Foundation
 
-use crate::models::events_models::events::EventStreamMessage;
+use crate::{
+    models::events_models::events::EventStreamMessage,
+    utils::counters::GRPC_TO_PROCESSOR_1_SERVE_LATENCY_IN_SECS,
+};
 use futures::{stream::SplitSink, SinkExt, StreamExt};
 use std::time::Duration;
 use tokio::{sync::broadcast::Receiver, time};
@@ -42,6 +45,14 @@ impl Stream {
                         );
                         panic!();
                     });
+                    GRPC_TO_PROCESSOR_1_SERVE_LATENCY_IN_SECS
+                        .with_label_values(&["event_stream"])
+                        .set({
+                            use chrono::TimeZone;
+                            let transaction_timestamp = chrono::Utc.from_utc_datetime(&event.transaction_timestamp);
+                            let transaction_timestamp = std::time::SystemTime::from(transaction_timestamp);
+                            std::time::SystemTime::now().duration_since(transaction_timestamp).unwrap_or_default().as_secs_f64()
+                        });
                     self.tx
                         .send(warp::ws::Message::text(serde_json::to_string(&event).unwrap()))
                         .await
