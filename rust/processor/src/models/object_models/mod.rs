8e91056e997c239f705d058eab1b5725c18f583c
CapCap <max@aptoslabs.com>
2024-02-26 10:25:56 -0800

    remove some more

--- rust/processor/src/models/object_models/mod.rs
+++
@@ -1,5 +0,0 @@
-// Copyright © Aptos Foundation
-// SPDX-License-Identifier: Apache-2.0
-
-pub mod v2_object_utils;
-pub mod v2_objects;
--- rust/processor/src/models/token_v2_models/mod.rs
+++
@@ -1,9 +0,0 @@
-// Copyright © Aptos Foundation
-// SPDX-License-Identifier: Apache-2.0
-
-pub mod v2_collections;
-pub mod v2_token_activities;
-pub mod v2_token_datas;
-pub mod v2_token_metadata;
-pub mod v2_token_ownerships;
-pub mod v2_token_utils;
