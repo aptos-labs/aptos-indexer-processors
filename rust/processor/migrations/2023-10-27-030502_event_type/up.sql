68650bdf4908f93a6031bd047ee013fa71405a0a
Patrick Nguyen <nguyenthaihoc.dev@gmail.com>
2024-03-07 13:17:15 +0700

    chore: update index

--- rust/processor/migrations/2023-10-27-030502_event_type/up.sql
+++ rust/processor/migrations/2023-10-27-030502_event_type/up.sql
@@ -1,7 +1,6 @@
 -- Your SQL goes here
 -- p99 currently is 303 so using 300 as a safe max length 
 ALTER TABLE events ADD COLUMN IF NOT EXISTS indexed_type VARCHAR(600) NOT NULL DEFAULT '';
--- CREATE INDEX IF NOT EXISTS ev_itype_index ON events (indexed_type);
 CREATE INDEX IF NOT EXISTS ev_itype_index_inserted_at ON events (indexed_type, inserted_at);
 CREATE TABLE IF NOT EXISTS spam_assets (
   asset VARCHAR(1100) PRIMARY KEY NOT NULL,
