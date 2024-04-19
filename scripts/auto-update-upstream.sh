cbc282ca7620e08ada875cea118440a1b97d091b
avbel <belchikovs@gmail.com>
2024-04-19 17:48:28 +0400

    addded git commit

--- scripts/auto-update-upstream.sh
+++ scripts/auto-update-upstream.sh
@@ -9,11 +9,15 @@ git remote add upstream "https://github.com/aptos-labs/aptos-indexer-processors.
 git fetch upstream
 git merge upstream/main
 
+cd rust
 cargo build --locked --release -p processor
 
 # Check if the build was successful
 if [ $? -eq 0 ]; then
     echo "Can be merged"
+    cd ..
+    git add .
+    git commit -m "Auto-merge $current_date"
 else
     echo "Build failed"
 fi
