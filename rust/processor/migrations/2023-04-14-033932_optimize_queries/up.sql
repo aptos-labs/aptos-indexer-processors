70ebc45a0566d82f86bf6723ef15ab20cb8a3903
Yuun Lim <dla0267@gmail.com>
2024-05-16 15:16:14 -0700

    replace move_resources view with account_transaction to deprecate move_resources

--- rust/processor/migrations/2023-04-14-033932_optimize_queries/up.sql
+++ rust/processor/migrations/2023-04-14-033932_optimize_queries/up.sql
@@ -6,11 +6,12 @@ SELECT account_address,
 FROM events
 GROUP BY 1,
   2;
--- need this to query transactions that touch an account's move resources
-CREATE OR REPLACE VIEW address_version_from_move_resources AS
-SELECT address,
+-- need this to query transactions that touch an account's move resources,
+-- previously used move_resources, and now it's using account_transactions to deprecate move_resources
+CREATE OR REPLACE VIEW address_version_from_account_transactions AS
+SELECT account_address,
   transaction_version
-FROM move_resources
+FROM account_transactions
 GROUP BY 1,
   2;
 -- need this for getting NFTs grouped by collections
