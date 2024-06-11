d52e4218e8b49ab4496ea4b9eb545ec3dedefdde
CapCap <max@aptoslabs.com>
2024-06-10 18:27:17 -0700

    rename small

--- rust/transaction-filter/src/filters/mod.rs
+++ rust/transaction-filter/src/filters/mod.rs
@@ -1,11 +1,11 @@
-pub mod event_filter;
+pub mod event;
 pub mod move_module;
 pub mod transaction_root;
 pub mod user_transaction_request;
 pub mod write_set_change_filter;
 
 // Re-export for easier use
-pub use event_filter::EventFilter;
+pub use event::EventFilter;
 pub use move_module::{MoveModuleFilter, MoveStructTagFilter};
 pub use transaction_root::TransactionRootFilter;
 pub use user_transaction_request::{UserTransactionPayloadFilter, UserTransactionRequestFilter};
