1082e0d4cb9c43181bfb23d3695ac84f3656c5af
rtso <8248583+rtso@users.noreply.github.com>
2024-06-26 19:32:40 -0400

    add if not exists

--- rust/processor/src/db/postgres/migrations/2024-06-26-220144_add_token_standard/up.sql
+++ rust/processor/src/db/postgres/migrations/2024-06-26-220144_add_token_standard/up.sql
@@ -1,6 +1,6 @@
 -- Your SQL goes here
 ALTER TABLE current_unified_fungible_asset_balances_to_be_renamed
-    ADD COLUMN token_standard VARCHAR(10) GENERATED ALWAYS AS (
+    ADD COLUMN IF NOT EXISTS token_standard VARCHAR(10) GENERATED ALWAYS AS (
         CASE 
             WHEN asset_type_v2 IS NOT NULL THEN 'v2'
             ELSE 'v1'
