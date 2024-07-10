9bad0be88f9afa3bc26dd55aceff3a01018ee6c7
yuunlimm <dla0267@gmail.com>
2024-07-10 13:16:22 -0700

    remove file and directly write buffer

--- rust/processor/src/bq_analytics/generic_parquet_processor.rs
+++ rust/processor/src/bq_analytics/generic_parquet_processor.rs
@@ -9,22 +9,16 @@ use crate::{
 };
 use ahash::AHashMap;
 use allocative::Allocative;
-use anyhow::Result;
+use anyhow::{Context, Result};
 use google_cloud_storage::client::Client as GCSClient;
 use parquet::{
     file::{properties::WriterProperties, writer::SerializedFileWriter},
     record::RecordWriter,
     schema::types::Type,
 };
-use std::{
-    fs::{remove_file, rename, File},
-    path::PathBuf,
-    sync::Arc,
-    time::Instant,
-};
+use std::{path::PathBuf, sync::Arc, time::Instant};
 use tokio::time::Duration;
 use tracing::{debug, error, info};
-use uuid::Uuid;
 
 #[derive(Debug, Default, Clone)]
 pub struct ParquetDataGeneric<ParquetType> {
@@ -66,7 +60,7 @@ where
     for<'a> &'a [ParquetType]: RecordWriter<ParquetType>,
 {
     pub schema: Arc<Type>,
-    pub writer: SerializedFileWriter<File>,
+    pub writer: SerializedFileWriter<Vec<u8>>,
     pub buffer: Vec<ParquetType>,
     pub buffer_size_bytes: usize,
 
@@ -74,30 +68,17 @@ where
     pub bucket_name: String,
     pub bucket_root: String,
     pub gap_detector_sender: kanal::AsyncSender<ProcessingResult>,
-    pub file_path: String,
     pub upload_interval: Duration,
     pub max_buffer_size: usize,
     pub last_upload_time: Instant,
 }
-fn create_new_writer(
-    file_path: &str,
-    schema: Arc<parquet::schema::types::Type>,
-) -> Result<SerializedFileWriter<File>> {
+fn create_new_writer(schema: Arc<Type>) -> Result<SerializedFileWriter<Vec<u8>>> {
     let props = WriterProperties::builder()
         .set_compression(parquet::basic::Compression::LZ4)
         .build();
     let props_arc = Arc::new(props);
-    let file: File = File::options()
-        .create(true)
-        .truncate(true)
-        .write(true)
-        .open(file_path)?;
 
-    Ok(SerializedFileWriter::new(
-        file.try_clone()?,
-        schema,
-        props_arc,
-    )?)
+    SerializedFileWriter::new(Vec::new(), schema, props_arc).context("Failed to create new writer")
 }
 
 impl<ParquetType> ParquetHandler<ParquetType>
@@ -105,16 +86,14 @@ where
     ParquetType: Allocative + GetTimeStamp + HasVersion + HasParquetSchema + 'static + NamedTable,
     for<'a> &'a [ParquetType]: RecordWriter<ParquetType>,
 {
-    fn create_new_writer(&self) -> Result<SerializedFileWriter<File>> {
-        let file_path = &self.file_path;
-        create_new_writer(file_path, self.schema.clone())
+    fn create_new_writer(&self) -> Result<SerializedFileWriter<Vec<u8>>> {
+        create_new_writer(self.schema.clone())
     }
 
-    fn close_writer(&mut self) -> Result<()> {
-        let mut writer = self.create_new_writer()?;
-        std::mem::swap(&mut self.writer, &mut writer);
-        writer.close()?;
-        Ok(())
+    fn close_writer(&mut self) -> Result<SerializedFileWriter<Vec<u8>>> {
+        let new_writer = self.create_new_writer()?;
+        let old_writer = std::mem::replace(&mut self.writer, new_writer);
+        Ok(old_writer)
     }
 
     pub fn new(
@@ -126,8 +105,7 @@ where
         max_buffer_size: usize,
     ) -> Result<Self> {
         // had to append unique id to avoid concurrent write issues
-        let file_path = format!("{}_{}.parquet", ParquetType::TABLE_NAME, Uuid::new_v4());
-        let writer = create_new_writer(&file_path, schema.clone())?;
+        let writer = create_new_writer(schema.clone())?;
 
         Ok(Self {
             writer,
@@ -138,7 +116,6 @@ where
             bucket_root,
             gap_detector_sender,
             schema,
-            file_path,
             upload_interval,
             max_buffer_size,
             last_upload_time: Instant::now(),
@@ -194,30 +171,40 @@ where
         if self.buffer.is_empty() {
             return Ok(());
         }
-        let start_version = self.buffer.first().unwrap().version();
-        let last = self.buffer.last().unwrap();
+        let start_version = self
+            .buffer
+            .first()
+            .context("Buffer is not empty but has no first element")?
+            .version();
+        let last = self
+            .buffer
+            .last()
+            .context("Buffer is not empty but has no last element")?;
         let end_version = last.version();
         let last_transaction_timestamp = naive_datetime_to_timestamp(last.get_timestamp());
 
         let txn_version_to_struct_count =
             process_struct_count_map(&self.buffer, &mut self.transaction_version_to_struct_count);
 
-        let new_file_path: PathBuf = PathBuf::from(format!(
-            "{}_{}.parquet",
-            ParquetType::TABLE_NAME,
-            Uuid::new_v4()
-        ));
-        rename(&self.file_path, &new_file_path)?; // this fixes an issue with concurrent file access issues
-
         let struct_buffer = std::mem::take(&mut self.buffer);
 
-        let mut row_group_writer = self.writer.next_row_group()?;
+        let mut row_group_writer = self
+            .writer
+            .next_row_group()
+            .context("Failed to get row group")?;
+
         struct_buffer
             .as_slice()
             .write_to_row_group(&mut row_group_writer)
-            .unwrap();
-        row_group_writer.close()?;
-        self.close_writer()?;
+            .context("Failed to write to row group")?;
+        row_group_writer
+            .close()
+            .context("Failed to close row group")?;
+
+        let old_writer = self.close_writer().context("Failed to close writer")?;
+        let upload_buffer = old_writer
+            .into_inner()
+            .context("Failed to get inner buffer")?;
 
         debug!(
             table_name = ParquetType::TABLE_NAME,
@@ -226,9 +213,10 @@ where
             "Max buffer size reached, uploading to GCS."
         );
         let bucket_root = PathBuf::from(&self.bucket_root);
+
         upload_parquet_to_gcs(
             gcs_client,
-            &new_file_path,
+            upload_buffer,
             ParquetType::TABLE_NAME,
             &self.bucket_name,
             &bucket_root,
@@ -236,7 +224,6 @@ where
         .await?;
 
         self.buffer_size_bytes = 0;
-        remove_file(&new_file_path)?;
 
         let parquet_processing_result = ParquetProcessingResult {
             start_version,
