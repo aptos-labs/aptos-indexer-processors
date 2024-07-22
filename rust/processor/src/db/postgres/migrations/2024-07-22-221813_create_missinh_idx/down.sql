58d7f8a0ae477f92e27505edcfaef5ce7d6e65ed
yuunlimm <dla0267@gmail.com>
2024-07-22 15:31:48 -0700

    add missing index causing query to timeout

---
+++ rust/processor/src/db/postgres/migrations/2024-07-22-221813_create_missinh_idx/down.sql
@@ -0,0 +1,5 @@
+-- This file should undo anything in `up.sql`
+SELECT 1;
+--DROP INDEX IF EXISTS to2_insat_index;
+--DROP INDEX IF EXISTS fab_insat_index;
+--DROP INDEX IF EXISTS o_insat_idx;
---
+++ rust/processor/src/db/postgres/migrations/2024-07-22-221813_create_missinh_idx/up.sql
@@ -0,0 +1,5 @@
+-- Your SQL goes here
+SELECT 1;
+--CREATE INDEX CONCURRENTLY IF NOT EXISTS to2_insat_index ON token_ownerships_v2 (inserted_at);
+--CREATE INDEX CONCURRENTLY IF NOT EXISTS fab_insat_index ON fungible_asset_balances (inserted_at);
+--CREATE INDEX CONCURRENTLY IF NOT EXISTS o_insat_idx ON objects (inserted_at);
