4ee1abb6f72aea41fb73ac3ebcda52cbdfaf34df
yuunlimm <dla0267@gmail.com>
2024-08-02 14:10:54 -0700

    lint

--- rust/integration-tests/src/simple_tests.rs
+++ rust/integration-tests/src/simple_tests.rs
@@ -6,7 +6,9 @@ mod test {
         ACCOUNT_B_TRANSFER_TO_ACCOUNT_B,
     };
     use diesel::{
-        pg::PgConnection, query_dsl::methods::{FilterDsl, SelectDsl}, sql_types::{BigInt, Text}, ExpressionMethods, RunQueryDsl
+        pg::PgConnection,
+        query_dsl::methods::{FilterDsl, SelectDsl},
+        ExpressionMethods, RunQueryDsl,
     };
     use processor::schema::events::dsl::*;
 
@@ -24,17 +26,17 @@ mod test {
         };
         assert!(test_context
             .run(processor_config, |conn: &mut PgConnection| {
-                let result =
-                    events.select((transaction_version, event_index, type_))
-                        .filter(transaction_version.eq(0))
-                        .load::<(i64, i64, String)>(conn);
+                let result = events
+                    .select((transaction_version, event_index, type_))
+                    .filter(transaction_version.eq(0))
+                    .load::<(i64, i64, String)>(conn);
                 assert_eq!(result.unwrap().len(), 5);
 
                 let withdraw_events = events
                     .select((transaction_version, event_index, type_))
                     .filter(type_.eq("0x1::coin::WithdrawEvent"))
                     .load::<(i64, i64, String)>(conn);
-                
+
                 // Withdraw from Faucet, Withdraw from AccountA, Withdraw from AccountB
                 assert_eq!(withdraw_events.unwrap().len(), 3);
                 Ok(())
