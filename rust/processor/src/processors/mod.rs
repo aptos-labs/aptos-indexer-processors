3b93e64b436b42c54f1be2bcaa16223dc0db3cb2
bowenyang007 <carlyang007@gmail.com>
2024-08-27 15:11:27 -0700

    lint

--- rust/processor/src/processors/mod.rs
+++ rust/processor/src/processors/mod.rs
@@ -155,22 +155,22 @@ pub trait ProcessorTrait: Send + Sync + Debug {
 }
 
 /// This enum captures the configs for all the different processors that are defined.
-/// 
+///
 /// The configs for each processor should only contain configuration specific to that
 /// processor. For configuration that is common to all processors, put it in
 /// IndexerGrpcProcessorConfig.
 #[derive(Clone, Debug, Deserialize, Serialize, strum::IntoStaticStr, strum::EnumDiscriminants)]
 #[serde(tag = "type", rename_all = "snake_case")]
-// What is all this strum stuff? Let me explain.
-//
-// Previously we had consts called NAME in each module and a function called `name` on
-// the ProcessorTrait. As such it was possible for this name to not match the snake case
-// representation of the struct name. By using strum we can have a single source for
-// processor names derived from the enum variants themselves.
-//
-// That's what this strum_discriminants stuff is, it uses macro magic to generate the
-// ProcessorName enum based on ProcessorConfig. The rest of the derives configure this
-// generation logic, e.g. to make sure we use snake_case.
+/// What is all this strum stuff? Let me explain.
+///
+/// Previously we had consts called NAME in each module and a function called `name` on
+/// the ProcessorTrait. As such it was possible for this name to not match the snake case
+/// representation of the struct name. By using strum we can have a single source for
+/// processor names derived from the enum variants themselves.
+///
+/// That's what this strum_discriminants stuff is, it uses macro magic to generate the
+/// ProcessorName enum based on ProcessorConfig. The rest of the derives configure this
+/// generation logic, e.g. to make sure we use snake_case.
 #[strum(serialize_all = "snake_case")]
 #[strum_discriminants(
     derive(
@@ -228,10 +228,10 @@ impl ProcessorConfig {
 }
 
 /// This enum contains all the processors defined in this crate.
-/// 
+///
 /// We use enum_dispatch as it is more efficient than using dynamic dispatch (Box<dyn ProcessorTrait>) and
 /// it enables nice safety checks like in we do in `test_processor_names_complete`.
-/// 
+///
 /// // To ensure that the variants of ProcessorConfig and Processor line up, in the testing
 // build path we derive EnumDiscriminants on this enum as well and make sure the two
 // sets of variants match up in `test_processor_names_complete`.
@@ -275,7 +275,7 @@ mod test {
 
     /// This test exists to make sure that when a new processor is added, it is added
     /// to both Processor and ProcessorConfig.
-    /// 
+    ///
     /// To make sure this passes, make sure the variants are in the same order
     /// (lexicographical) and the names match.
     #[test]
