4091f14922347b77b8f670efbeb504cc0c2a7885
bowenyang007 <carlyang007@gmail.com>
2024-08-27 21:38:17 -0700

    add index back

--- rust/processor/src/db/postgres/migrations/2024-08-28-015552_fa_migration_fix_2/up.sql
+++ rust/processor/src/db/postgres/migrations/2024-08-28-015552_fa_migration_fix_2/up.sql
@@ -1,6 +1,7 @@
 -- Your SQL goes here
 ALTER TABLE current_unified_fungible_asset_balances_to_be_renamed
 ADD COLUMN IF NOT EXISTS asset_type VARCHAR(1000) GENERATED ALWAYS AS (COALESCE(asset_type_v1, asset_type_v2)) STORED;
+CREATE INDEX IF NOT EXISTS cufab_owner_at_index ON current_unified_fungible_asset_balances_to_be_renamed (owner_address, asset_type);
 ALTER TABLE current_unified_fungible_asset_balances_to_be_renamed
 ADD COLUMN IF NOT EXISTS token_standard VARCHAR(10) GENERATED ALWAYS AS (
     CASE
