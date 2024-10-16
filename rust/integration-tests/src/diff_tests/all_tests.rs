b95f3dae342102b98c971767711d0a7cd98eff88
yuunlimm <dla0267@gmail.com>
2024-10-16 14:55:44 -0700

    Update existing processor tests with the latest dependency change

--- rust/integration-tests/src/diff_tests/all_tests.rs
+++ rust/integration-tests/src/diff_tests/all_tests.rs
@@ -4,14 +4,11 @@ mod test {
 
     use crate::{
         diff_test_helper::{
-            processors::{
-                event_processor::EventsProcessorTestHelper,
-                fungible_asset_processor::FungibleAssetProcessorTestHelper,
-                token_v2_processor::TokenV2ProcessorTestHelper,
-            },
-            ProcessorTestHelper,
+            event_processor::load_data as load_event_data,
+            fungible_asset_processor::load_data as load_fungible_asset_data,
+            token_v2_processor::load_data as load_token_v2_data,
         },
-        diff_tests::{
+        diff_tests::diff_tests_helper::{
             get_expected_imported_mainnet_txns, get_expected_imported_testnet_txns,
             get_expected_scripted_txns, remove_inserted_at, remove_transaction_timestamp,
         },
@@ -20,19 +17,31 @@ mod test {
     use aptos_indexer_test_transactions::{
         ALL_IMPORTED_MAINNET_TXNS, ALL_IMPORTED_TESTNET_TXNS, ALL_SCRIPTED_TRANSACTIONS,
     };
+    use aptos_indexer_testing_framework::{
+        cli_parser::get_test_config, sdk_test_context::generate_output_file,
+    };
     use assert_json_diff::assert_json_eq;
     use diesel::pg::PgConnection;
     use processor::processors::token_v2_processor::TokenV2ProcessorConfig;
-    use std::{collections::HashMap, fs, sync::Arc};
+    use std::fs;
+
+    const DEFAULT_OUTPUT_FOLDER: &str = "expected_db_output_files";
 
     #[tokio::test]
     async fn test_all_testnet_txns_schema_output_for_all_processors() {
+        let (generate_output_flag, custom_output_path) = get_test_config();
+        let output_path = custom_output_path
+            .unwrap_or_else(|| DEFAULT_OUTPUT_FOLDER.to_string() + "/imported_testnet_txns");
+
         let processor_configs = get_processor_configs();
         let test_context = TestContext::new(ALL_IMPORTED_TESTNET_TXNS).await.unwrap();
 
         run_processor_tests(
             processor_configs,
             &test_context,
+            generate_output_flag,
+            output_path,
+            false,
             get_expected_imported_testnet_txns,
         )
         .await;
@@ -40,12 +49,18 @@ mod test {
 
     #[tokio::test]
     async fn test_all_mainnet_txns_schema_output_for_all_processors() {
+        let (generate_output_flag, custom_output_path) = get_test_config();
+        let output_path = custom_output_path
+            .unwrap_or_else(|| DEFAULT_OUTPUT_FOLDER.to_string() + "/imported_mainnet_txns");
+
         let processor_configs = get_processor_configs();
         let test_context = TestContext::new(ALL_IMPORTED_MAINNET_TXNS).await.unwrap();
-
         run_processor_tests(
             processor_configs,
             &test_context,
+            generate_output_flag,
+            output_path,
+            false,
             get_expected_imported_mainnet_txns,
         )
         .await;
@@ -53,68 +68,107 @@ mod test {
 
     #[tokio::test]
     async fn test_all_scripted_txns_schema_output_for_all_processors() {
+        let (generate_output_flag, custom_output_path) = get_test_config();
+        let output_path = custom_output_path
+            .unwrap_or_else(|| DEFAULT_OUTPUT_FOLDER.to_string() + "/scripted_txns");
+
         let processor_configs = get_processor_configs();
         let test_context = TestContext::new(ALL_SCRIPTED_TRANSACTIONS).await.unwrap();
 
-        run_processor_tests(processor_configs, &test_context, get_expected_scripted_txns).await;
+        run_processor_tests(
+            processor_configs,
+            &test_context,
+            generate_output_flag,
+            output_path,
+            true,
+            get_expected_scripted_txns,
+        )
+        .await;
     }
 
     // Helper function to reduce duplicate code for running tests on all processors
     async fn run_processor_tests(
         processor_configs: Vec<TestProcessorConfig>,
         test_context: &TestContext,
-        get_expected_json_path_fn: fn(&str, &str) -> String,
+        generate_output_flag: bool,
+        output_path: String,
+        _scripted: bool,
+        get_expected_json_path_fn: fn(&str, &str, &str) -> String,
     ) {
-        let processor_map = get_processor_map();
         for processor_config in processor_configs {
             let processor_name = processor_config.config.name();
             let test_type = TestType::Diff(DiffTest);
+            let output_path = output_path.clone();
 
-            if let Some(test_helper) = processor_map.get(processor_name).cloned() {
-                test_context
+            let db_values_fn = match processor_name {
+                "events_processor" => load_event_data,
+                "fungible_asset_processor" => load_fungible_asset_data,
+                "token_v2_processor" => load_token_v2_data,
+                _ => panic!("Unknown processor: {}", processor_name),
+            };
+
+            test_context
                     .run(
                         processor_config,
                         test_type,
                         move |conn: &mut PgConnection, txn_version: &str| {
-                            let mut json_data = match test_helper.load_data(conn, txn_version) {
-                                Ok(data) => data,
+
+                            let mut db_values = match db_values_fn(conn, txn_version) {
+                                Ok(db_data) => db_data,
                                 Err(e) => {
                                     eprintln!(
                                         "[ERROR] Failed to load data for processor {} and transaction version {}: {}",
                                         processor_name, txn_version, e
                                     );
                                     return Err(e);
-                                }
+                                },
                             };
 
-                            let expected_json_path = get_expected_json_path_fn(processor_name, txn_version);
-                            let mut expected_json = match read_and_parse_json(&expected_json_path) {
-                                Ok(json) => json,
-                                Err(e) => {
-                                    eprintln!(
-                                        "[ERROR] Error handling JSON for processor {} and transaction version {}: {}",
-                                        processor_name, txn_version, e
-                                    );
-                                    return Err(e);
+                            // Iterate over each table in the map and validate its data
+                            for (table_name, db_value) in db_values.iter_mut() {
+                                if generate_output_flag {
+                                    println!("[TEST] Generating output files for all tables.");
+                                    remove_inserted_at(db_value);
+                                    // Iterate over each table's data in the HashMap and generate an output file
+                                    generate_output_file(
+                                        processor_name,
+                                        table_name,
+                                        txn_version,
+                                        db_value,
+                                        output_path.clone(),
+                                    )?;
                                 }
-                            };
 
-                            // TODO: we need to enhance json diff, as we might have more complex diffs.
-                            remove_inserted_at(&mut json_data);
-                            remove_transaction_timestamp(&mut json_data);
-                            remove_transaction_timestamp(&mut expected_json);
-                            assert_json_eq!(&json_data, &expected_json);
+                                // Generate the expected JSON file path for each table
+                                let expected_file_path = &get_expected_json_path_fn(processor_name, txn_version, table_name);
 
-                            println!(
-                                "[INFO] Test passed for processor {} and transaction version: {}",
-                                processor_name, txn_version
-                            );
+                                // Read and parse the expected JSON file for the current table
+                                let mut expected_json = match read_and_parse_json(expected_file_path) {
+                                    Ok(json) => json,
+                                    Err(e) => {
+                                        eprintln!(
+                                            "[ERROR] Error handling JSON for processor {} table {} and transaction version {}: {}",
+                                            processor_name, table_name, txn_version, e
+                                        );
+                                        panic!("Failed to read and parse JSON for table: {}", table_name);
+                                    },
+                                };
+
+                                // TODO: Clean up non-deterministic fields (e.g., timestamps, `inserted_at`)
+                                remove_inserted_at(db_value);
+                                remove_transaction_timestamp(db_value);
+                                remove_inserted_at(&mut expected_json);
+                                remove_transaction_timestamp(&mut expected_json);
+
+                                // Validate the actual vs expected JSON for the current table
+                                assert_json_eq!(db_value, expected_json);
+                            }
+
                             Ok(())
                         },
                     )
                     .await
                     .unwrap();
-            }
         }
     }
 
@@ -133,24 +187,6 @@ mod test {
                 Err(anyhow::anyhow!("Failed to read file: {}", e))
             },
         }
-    }
-
-    fn get_processor_map() -> HashMap<String, Arc<Box<dyn ProcessorTestHelper>>> {
-        let mut processor_map: HashMap<String, Arc<Box<dyn ProcessorTestHelper>>> = HashMap::new();
-        processor_map.insert(
-            "events_processor".to_string(),
-            Arc::new(Box::new(EventsProcessorTestHelper) as Box<dyn ProcessorTestHelper>),
-        );
-        processor_map.insert(
-            "fungible_asset_processor".to_string(),
-            Arc::new(Box::new(FungibleAssetProcessorTestHelper) as Box<dyn ProcessorTestHelper>),
-        );
-        processor_map.insert(
-            "token_v2_processor".to_string(),
-            Arc::new(Box::new(TokenV2ProcessorTestHelper) as Box<dyn ProcessorTestHelper>),
-        );
-
-        processor_map
     }
 
     fn get_processor_configs() -> Vec<TestProcessorConfig> {
--- rust/integration-tests/src/diff_tests/mod.rs
+++ rust/integration-tests/src/diff_tests/mod.rs
@@ -1,49 +1,59 @@
-use serde_json::Value;
-
 mod all_tests;
 
-#[allow(dead_code)]
-pub fn remove_inserted_at(value: &mut Value) {
-    if let Some(array) = value.as_array_mut() {
-        for item in array.iter_mut() {
-            if let Some(obj) = item.as_object_mut() {
-                obj.remove("inserted_at");
+#[cfg(test)]
+pub mod diff_tests_helper {
+    use serde_json::Value;
+
+    pub fn remove_inserted_at(value: &mut Value) {
+        if let Some(array) = value.as_array_mut() {
+            for item in array.iter_mut() {
+                if let Some(obj) = item.as_object_mut() {
+                    obj.remove("inserted_at");
+                }
             }
         }
     }
-}
 
-#[allow(dead_code)]
-pub fn remove_transaction_timestamp(value: &mut Value) {
-    if let Some(array) = value.as_array_mut() {
-        for item in array.iter_mut() {
-            if let Some(obj) = item.as_object_mut() {
-                obj.remove("transaction_timestamp");
+    pub fn remove_transaction_timestamp(value: &mut Value) {
+        if let Some(array) = value.as_array_mut() {
+            for item in array.iter_mut() {
+                if let Some(obj) = item.as_object_mut() {
+                    obj.remove("transaction_timestamp");
+                }
             }
         }
     }
-}
 
-#[allow(dead_code)]
-pub fn get_expected_imported_testnet_txns(processor_name: &str, txn_version: &str) -> String {
-    format!(
-        "expected_db_output_files/imported_testnet_txns/{}/{}_{}.json",
-        processor_name, processor_name, txn_version
-    )
-}
+    pub fn get_expected_imported_testnet_txns(
+        processor_name: &str,
+        txn_version: &str,
+        table_name: &str,
+    ) -> String {
+        format!(
+            "expected_db_output_files/imported_testnet_txns/{}/{}/{}.json",
+            processor_name, txn_version, table_name
+        )
+    }
 
-#[allow(dead_code)]
-pub fn get_expected_imported_mainnet_txns(processor_name: &str, txn_version: &str) -> String {
-    format!(
-        "expected_db_output_files/imported_mainnet_txns/{}/{}_{}.json",
-        processor_name, processor_name, txn_version
-    )
-}
+    pub fn get_expected_imported_mainnet_txns(
+        processor_name: &str,
+        txn_version: &str,
+        table_name: &str,
+    ) -> String {
+        format!(
+            "expected_db_output_files/imported_mainnet_txns/{}/{}/{}.json",
+            processor_name, txn_version, table_name
+        )
+    }
 
-#[allow(dead_code)]
-pub fn get_expected_scripted_txns(processor_name: &str, txn_version: &str) -> String {
-    format!(
-        "expected_db_output_files/scripted_txns/{}/{}_{}.json",
-        processor_name, processor_name, txn_version
-    )
+    pub fn get_expected_scripted_txns(
+        processor_name: &str,
+        txn_version: &str,
+        table_name: &str,
+    ) -> String {
+        format!(
+            "expected_db_output_files/scripted_txns/{}/{}/{}.json",
+            processor_name, txn_version, table_name
+        )
+    }
 }
