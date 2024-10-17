dd216814e4671486a67a06d822110d8b6337efad
yuunlimm <dla0267@gmail.com>
2024-10-17 10:03:59 -0700

    add db output diff tests for existing processors

--- rust/integration-tests/src/models/mod.rs
+++ rust/integration-tests/src/models/mod.rs
@@ -1,1 +1,8 @@
-pub mod queryable_models;
+pub mod ans_models;
+pub mod default_models;
+pub mod events_models;
+pub mod fa_v2_models;
+pub mod objects_models;
+pub mod stake_models;
+pub mod token_v2_models;
+pub mod user_transactions_models;
