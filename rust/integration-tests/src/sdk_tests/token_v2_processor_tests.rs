643d0ec77aca4ef5395b30dd67895b1d5095f616
rtso <8248583+rtso@users.noreply.github.com>
2024-11-04 16:39:05 -0500

    token v1 tests

--- rust/integration-tests/src/sdk_tests/token_v2_processor_tests.rs
+++ rust/integration-tests/src/sdk_tests/token_v2_processor_tests.rs
@@ -97,15 +97,11 @@ mod sdk_token_v2_processor_tests {
     /**
     * This test includes processing for the following:
     * - Resources
-    *      - 0x4::aptos_token::AptosCollection
     *      - 0x4::collection::Collection
-    *      - 0x4::collection::ConcurrentSupply
-    *      - 0x4::aptos_token::AptosToken
-    *      - 0x4::property_map::PropertyMap
-    *      - 0x4::token::Token
-
+    *      - 0x4::collection::UnlimitedSupply
     * - Events
-    *      - 0x4::collection::Mint
+    *      - 0x4::collection::BurnEvent
+
     */
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
     async fn test_token_v2_burn_event_v1() {
@@ -117,18 +113,15 @@ mod sdk_token_v2_processor_tests {
     }
 
     /**
-    * This test includes processing for the following:
-    * - Resources
-    *      - 0x4::aptos_token::AptosCollection
-    *      - 0x4::collection::Collection
-    *      - 0x4::collection::ConcurrentSupply
-    *      - 0x4::aptos_token::AptosToken
-    *      - 0x4::property_map::PropertyMap
-    *      - 0x4::token::Token
-
-    * - Events
-    *      - 0x4::collection::Mint
-    */
+     * This test includes processing for the following:
+     * - Resources
+     *      - 0x4::collection::UnlimitedSupply
+     *      - 0x4::collection::Collection
+     *      - 0x4::token::Token
+     * - Events
+     *      - 0x4::collection::MintEvent
+     *      - 0x1::object::TransferEvent
+     */
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
     async fn test_token_v2_unlimited_supply() {
         process_single_transaction(
@@ -143,13 +136,14 @@ mod sdk_token_v2_processor_tests {
     * - Resources
     *      - 0x4::aptos_token::AptosCollection
     *      - 0x4::collection::Collection
-    *      - 0x4::collection::ConcurrentSupply
+    *      - 0x4::collection::FixedSupply
     *      - 0x4::aptos_token::AptosToken
     *      - 0x4::property_map::PropertyMap
     *      - 0x4::token::Token
 
     * - Events
-    *      - 0x4::collection::Mint
+     *      - 0x4::collection::MintEvent
+     *      - 0x1::object::TransferEvent
     */
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
     async fn test_token_v2_mint_and_transfer_event_v1() {
@@ -165,13 +159,13 @@ mod sdk_token_v2_processor_tests {
     * - Resources
     *      - 0x4::aptos_token::AptosCollection
     *      - 0x4::collection::Collection
-    *      - 0x4::collection::ConcurrentSupply
+    *      - 0x4::collection::FixedSupply
     *      - 0x4::aptos_token::AptosToken
     *      - 0x4::property_map::PropertyMap
     *      - 0x4::token::Token
 
     * - Events
-    *      - 0x4::collection::Mint
+    *      - 0x4::collection::MintEvent
     */
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
     async fn test_token_v2_fixed_supply() {
@@ -188,12 +182,9 @@ mod sdk_token_v2_processor_tests {
     *      - 0x4::aptos_token::AptosCollection
     *      - 0x4::collection::Collection
     *      - 0x4::collection::ConcurrentSupply
-    *      - 0x4::aptos_token::AptosToken
-    *      - 0x4::property_map::PropertyMap
-    *      - 0x4::token::Token
-
     * - Events
-    *      - 0x4::collection::Mint
+    *      - 0x4::collection::Burn
+
     */
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
     async fn test_token_v2_burn_event_v2() {
@@ -207,15 +198,12 @@ mod sdk_token_v2_processor_tests {
     /**
     * This test includes processing for the following:
     * - Resources
-    *      - 0x4::aptos_token::AptosCollection
-    *      - 0x4::collection::Collection
-    *      - 0x4::collection::ConcurrentSupply
-    *      - 0x4::aptos_token::AptosToken
     *      - 0x4::property_map::PropertyMap
     *      - 0x4::token::Token
-
+    *      - 0x4::token::TokenIdentifiers
     * - Events
-    *      - 0x4::collection::Mint
+    *      - 0x4::token::MutationEvent
+
     */
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
     async fn test_token_v2_mutation_event() {
