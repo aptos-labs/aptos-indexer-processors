76f684153f2b716b60b5c9e4534b0b3a54630cb5
Junha Park <0xjunha@gmail.com>
2024-12-04 16:34:45 +0900

    Add account restoration processor

--- rust/processor/src/db/postgres/models/account_restoration_models/public_key_auth_keys.rs
+++ rust/processor/src/db/postgres/models/account_restoration_models/public_key_auth_keys.rs
@@ -3,16 +3,15 @@
 
 #![allow(clippy::extra_unused_lifetimes)]
 
-use crate::{
-    schema::public_key_auth_keys::{self},
-};
+use crate::schema::public_key_auth_keys::{self};
+use field_count::FieldCount;
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, Deserialize, Identifiable, Insertable, Serialize)]
-#[diesel(primary_key(public_key_bcs, public_key_type, auth_key))]
+#[derive(Clone, Debug, Default, Deserialize, FieldCount, Identifiable, Insertable, Serialize)]
+#[diesel(primary_key(public_key, public_key_type, auth_key))]
 #[diesel(table_name = public_key_auth_keys)]
 pub struct PublicKeyAuthKey {
-    pub public_key_bcs: String,
+    pub public_key: String,
     pub public_key_type: String,
     pub auth_key: String,
     pub verified: bool,
