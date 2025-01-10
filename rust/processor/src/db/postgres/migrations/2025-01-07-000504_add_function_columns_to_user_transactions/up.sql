c5ebe52dcbae8942d255541fdfcd73d0ab9df71d
rtso <8248583+rtso@users.noreply.github.com>
2025-01-10 16:16:16 -0500

    make index creation optional

--- rust/processor/src/db/postgres/migrations/2025-01-07-000504_add_function_columns_to_user_transactions/up.sql
+++ rust/processor/src/db/postgres/migrations/2025-01-07-000504_add_function_columns_to_user_transactions/up.sql
@@ -3,4 +3,6 @@ ALTER TABLE user_transactions
 ADD COLUMN entry_function_contract_address VARCHAR(66),
 ADD COLUMN entry_function_module_name VARCHAR(255),
 ADD COLUMN entry_function_function_name VARCHAR(255);
-CREATE INDEX IF NOT EXISTS user_transactions_contract_info_index ON user_transactions (entry_function_contract_address, entry_function_module_name, entry_function_function_name);
+
+-- If you would like to run these indices, please do it outside of diesel migration since it will be blocking processing
+-- CREATE INDEX CONCURRENTLY IF NOT EXISTS user_transactions_contract_info_index ON user_transactions (entry_function_contract_address, entry_function_module_name, entry_function_function_name);
