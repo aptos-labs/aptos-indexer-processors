a403d68d34bf907e3ebe97195797d8164601c0fd
rtso <8248583+rtso@users.noreply.github.com>
2025-01-10 13:16:29 -0500

    remove null constraint

--- rust/processor/src/db/postgres/migrations/2025-01-07-000504_add_function_columns_to_user_transactions/up.sql
+++ rust/processor/src/db/postgres/migrations/2025-01-07-000504_add_function_columns_to_user_transactions/up.sql
@@ -1,6 +1,6 @@
 -- Your SQL goes here
 ALTER TABLE user_transactions
-ADD COLUMN entry_function_contract_address VARCHAR(66) NOT NULL,
-ADD COLUMN entry_function_module_name VARCHAR(255) NOT NULL,
-ADD COLUMN entry_function_function_name VARCHAR(255) NOT NULL;
+ADD COLUMN entry_function_contract_address VARCHAR(66),
+ADD COLUMN entry_function_module_name VARCHAR(255),
+ADD COLUMN entry_function_function_name VARCHAR(255);
 CREATE INDEX IF NOT EXISTS user_transactions_contract_info_index ON user_transactions (entry_function_contract_address, entry_function_module_name, entry_function_function_name);
