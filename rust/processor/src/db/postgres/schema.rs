9a8cf0d0ea7c27a0b0e7ac68af5a5cb73907a23a
bowenyang007 <carlyang007@gmail.com>
2025-01-30 23:34:48 -0800

    create schema for new mapping table

--- rust/processor/src/db/postgres/schema.rs
+++ rust/processor/src/db/postgres/schema.rs
@@ -218,6 +218,16 @@ diesel::table! {
 }
 
 diesel::table! {
+    coin_to_fungible_asset_mappings (coin_type) {
+        #[max_length = 1000]
+        coin_type -> Varchar,
+        #[max_length = 66]
+        fungible_asset_metadata_address -> Varchar,
+        last_transaction_version -> Int8,
+    }
+}
+
+diesel::table! {
     collection_datas (collection_data_id_hash, transaction_version) {
         #[max_length = 64]
         collection_data_id_hash -> Varchar,
@@ -882,6 +892,16 @@ diesel::table! {
 }
 
 diesel::table! {
+    fungible_asset_to_coin_mappings (fungible_asset_metadata_address) {
+        #[max_length = 66]
+        fungible_asset_metadata_address -> Varchar,
+        #[max_length = 1000]
+        coin_type -> Varchar,
+        last_transaction_version -> Int8,
+    }
+}
+
+diesel::table! {
     indexer_status (db) {
         #[max_length = 50]
         db -> Varchar,
@@ -1349,6 +1369,7 @@ diesel::allow_tables_to_appear_in_same_query!(
     coin_balances,
     coin_infos,
     coin_supply,
+    coin_to_fungible_asset_mappings,
     collection_datas,
     collections_v2,
     current_ans_lookup,
@@ -1382,6 +1403,7 @@ diesel::allow_tables_to_appear_in_same_query!(
     fungible_asset_activities,
     fungible_asset_balances,
     fungible_asset_metadata,
+    fungible_asset_to_coin_mappings,
     indexer_status,
     ledger_infos,
     move_modules,
