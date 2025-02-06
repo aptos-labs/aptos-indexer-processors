e943d0fb8e6b7f196a5a6eb666f7d7ba8c55174b
Yuun Lim <38443641+yuunlimm@users.noreply.github.com>
2025-02-06 13:23:59 -0800

    support multi txn test (#703)
    ### Description
    This is to help testing a sequential multi txn tests particularly for FA processor at the moment. this is done by refactoring some of codes we already have. Which means we still support a single batch of multi txns at the level of Testing Framework.
    
    
    
    
    We will be working on some improvements to testing framework to:
    1. Support better file generation (right now it creates two folders for one test case in the same level)
    2. Support Sequential Multi txn Testing at the level of testing framework. 
    
    Test cases That Testing Framework supports:
    
    ### A: Sequential Multi Txn testing 
    you have multiple txns as an input,
    processor processing order in two or more processing. 
    
    
    Example:
    ```
    Input: [txn1, txn2]
    
    txn1 from Grpc, processor processes txn1 
    txn2  from Grpc,  processor processes txn2
    ```
    
    ### B: Single Batch of multi txn testing 
    
    Example:
    ```
    input: [txn1, txn2]
    
    1. txn1, txn2, process
    2. txn3, process 
    ```
    
    ### C. Combo of A and B:
    Example:
    ```
    input: [txn1, txn2, txn3, ... , n]
    
    [txn1, txn2, txn3, ... , n] and processor processes
    ```

--- rust/integration-tests/src/sdk_tests/fungible_asset_processor_tests.rs
+++ rust/integration-tests/src/sdk_tests/fungible_asset_processor_tests.rs
@@ -45,7 +45,7 @@ mod sdk_fungible_asset_processor_tests {
         diff_test_helper::fungible_asset_processor::load_data,
         sdk_tests::{
             fungible_asset_processor_tests::setup_fa_processor_config, run_processor_test,
-            setup_test_environment, validate_json, DEFAULT_OUTPUT_FOLDER,
+            validate_json, DEFAULT_OUTPUT_FOLDER,
         },
     };
     use aptos_indexer_test_transactions::json_transactions::generated_transactions::{
@@ -54,6 +54,8 @@ mod sdk_fungible_asset_processor_tests {
         IMPORTED_MAINNET_TXNS_1737056775_COIN_TRANSFER_BURN_EVENT,
         IMPORTED_MAINNET_TXNS_1957950162_FA_MIGRATION_V2_STORE_ONLY,
         IMPORTED_MAINNET_TXNS_2186504987_COIN_STORE_DELETION_NO_EVENT,
+        IMPORTED_MAINNET_TXNS_2308282694_ASSET_TYPE_V1_NULL,
+        IMPORTED_MAINNET_TXNS_2308283617_ASSET_TYPE_V1_NULL_2,
         IMPORTED_MAINNET_TXNS_255894550_STORAGE_REFUND,
         IMPORTED_MAINNET_TXNS_508365567_FA_V1_EVENTS,
         IMPORTED_MAINNET_TXNS_550582915_MULTIPLE_TRANSFER_EVENT,
@@ -65,13 +67,17 @@ mod sdk_fungible_asset_processor_tests {
         IMPORTED_TESTNET_TXNS_5979639459_COIN_REGISTER,
         IMPORTED_TESTNET_TXNS_5992795934_FA_ACTIVITIES, IMPORTED_TESTNET_TXNS_646928741_NO_EVENTS,
     };
-    use aptos_indexer_testing_framework::{cli_parser::get_test_config, database::TestDatabase};
+    use aptos_indexer_testing_framework::{
+        cli_parser::get_test_config,
+        database::{PostgresTestDatabase, TestDatabase},
+        sdk_test_context::SdkTestContext,
+    };
     use sdk_processor::processors::fungible_asset_processor::FungibleAssetProcessor;
 
     // Test case for processing a specific testnet transaction (Validator Transaction)
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
     async fn test_fungible_asset_processor_validator_txn() {
-        process_fa_txns(
+        process_single_batch_txns(
             &[IMPORTED_TESTNET_TXNS_5523474016_VALIDATOR_TXN],
             Some("validator_txn_test".to_string()),
         )
@@ -81,7 +87,7 @@ mod sdk_fungible_asset_processor_tests {
     // Test case for processing another testnet transaction (Coin Register)
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
     async fn test_fungible_asset_processor_coin_register_txn() {
-        process_fa_txns(
+        process_single_batch_txns(
             &[IMPORTED_TESTNET_TXNS_5979639459_COIN_REGISTER],
             Some("coin_register_txn_test".to_string()),
         )
@@ -90,7 +96,7 @@ mod sdk_fungible_asset_processor_tests {
 
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
     async fn test_fungible_asset_processor_fa_activities_txn() {
-        process_fa_txns(
+        process_single_batch_txns(
             &[IMPORTED_TESTNET_TXNS_5992795934_FA_ACTIVITIES],
             Some("fa_activities_txn_test".to_string()),
         )
@@ -112,7 +118,7 @@ mod sdk_fungible_asset_processor_tests {
      */
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
     async fn test_fungible_asset_processor_coin_and_fa_transfers() {
-        process_fa_txns(
+        process_single_batch_txns(
             &[IMPORTED_MAINNET_TXNS_999929475_COIN_AND_FA_TRANSFERS],
             Some("coin_and_fa_transfers_test".to_string()),
         )
@@ -128,7 +134,7 @@ mod sdk_fungible_asset_processor_tests {
      */
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
     async fn test_fungible_asset_processor_v1_events() {
-        process_fa_txns(
+        process_single_batch_txns(
             &[IMPORTED_MAINNET_TXNS_508365567_FA_V1_EVENTS],
             Some("v1_events_test".to_string()),
         )
@@ -142,7 +148,7 @@ mod sdk_fungible_asset_processor_tests {
      */
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
     async fn test_fungible_asset_processor_v2_frozen_event() {
-        process_fa_txns(
+        process_single_batch_txns(
             &[IMPORTED_TESTNET_TXNS_1200394037_FA_V2_FROZEN_EVENT],
             Some("v2_frozen_event_test".to_string()),
         )
@@ -157,7 +163,7 @@ mod sdk_fungible_asset_processor_tests {
      */
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
     async fn test_fungible_asset_processor_concurrent_fa() {
-        process_fa_txns(
+        process_single_batch_txns(
             &[IMPORTED_TESTNET_TXNS_2646510387_CONCURRENT_FA],
             Some("concurrent_fa_test".to_string()),
         )
@@ -168,7 +174,7 @@ mod sdk_fungible_asset_processor_tests {
     /// Validates the handling of updated coin event formats
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
     async fn test_fungible_asset_processor_coin_v2_events() {
-        process_fa_txns(
+        process_single_batch_txns(
             &[IMPORTED_DEVNET_TXNS_78753811_COIN_TRANSFER_WITH_V2_EVENTS],
             Some("coin_v2_events".to_string()),
         )
@@ -177,7 +183,7 @@ mod sdk_fungible_asset_processor_tests {
 
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
     async fn test_fungible_asset_processor_coin_store_deletion_no_event() {
-        process_fa_txns(
+        process_single_batch_txns(
             &[IMPORTED_MAINNET_TXNS_2186504987_COIN_STORE_DELETION_NO_EVENT],
             Some("coin_store_deletion_no_event".to_string()),
         )
@@ -188,7 +194,7 @@ mod sdk_fungible_asset_processor_tests {
     /// Validates correct handling of burning tokens from secondary stores
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
     async fn test_fungible_asset_processor_secondary_store_burnt() {
-        process_fa_txns(
+        process_single_batch_txns(
             &[IMPORTED_TESTNET_TXNS_4462417704_SECONDARY_STORE_BURNT],
             Some("secondary_store_burnt".to_string()),
         )
@@ -199,7 +205,7 @@ mod sdk_fungible_asset_processor_tests {
     /// Validates correct handling of isolated gas events
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
     async fn test_fungible_asset_processor_gas_event_when_events_is_empty() {
-        process_fa_txns(
+        process_single_batch_txns(
             &[IMPORTED_TESTNET_TXNS_646928741_NO_EVENTS],
             Some("gas_event_when_events_is_empty".to_string()),
         )
@@ -210,7 +216,7 @@ mod sdk_fungible_asset_processor_tests {
     /// Validates handling of burn operations during coin transfers
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
     async fn test_fungible_asset_processor_coin_transfer_burn_event() {
-        process_fa_txns(
+        process_single_batch_txns(
             &[IMPORTED_MAINNET_TXNS_1737056775_COIN_TRANSFER_BURN_EVENT],
             Some("coin_transfer_burn_event".to_string()),
         )
@@ -221,7 +227,7 @@ mod sdk_fungible_asset_processor_tests {
     /// Validates correct handling of batch transfers
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
     async fn test_fungible_asset_processor_multiple_transfer_event() {
-        process_fa_txns(
+        process_single_batch_txns(
             &[IMPORTED_MAINNET_TXNS_550582915_MULTIPLE_TRANSFER_EVENT],
             Some("multiple_transfer_event".to_string()),
         )
@@ -232,7 +238,7 @@ mod sdk_fungible_asset_processor_tests {
     /// Validates correct handling of storage refund mechanics
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
     async fn test_fungible_asset_processor_storage_refund() {
-        process_fa_txns(
+        process_single_batch_txns(
             &[IMPORTED_MAINNET_TXNS_255894550_STORAGE_REFUND],
             Some("storage_refund".to_string()),
         )
@@ -244,7 +250,7 @@ mod sdk_fungible_asset_processor_tests {
     /// processes an FA version of that coin
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
     async fn test_fungible_asset_processor_fa_migration_same_batch() {
-        process_fa_txns(
+        process_single_batch_txns(
             &[
                 IMPORTED_MAINNET_TXNS_1680592683_FA_MIGRATION_COIN_INFO,
                 IMPORTED_MAINNET_TXNS_1957950162_FA_MIGRATION_V2_STORE_ONLY,
@@ -255,27 +261,67 @@ mod sdk_fungible_asset_processor_tests {
     }
 
     // TODO: I really want to make this work but it doesn't right now
-    // #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
-    // async fn test_fungible_asset_processor_fa_migration_different_batch() {
-    //     process_fa_txns(
-    //         &[IMPORTED_MAINNET_TXNS_1680592683_FA_MIGRATION_COIN_INFO],
-    //         Some("fa_migration_2_batch".to_string()),
-    //     )
-    //     .await;
+    #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
+    async fn test_fungible_asset_processor_fa_migration_different_batch() {
+        sequential_multi_transaction_helper_function(
+            &[
+                &[IMPORTED_MAINNET_TXNS_1680592683_FA_MIGRATION_COIN_INFO],
+                &[IMPORTED_MAINNET_TXNS_1957950162_FA_MIGRATION_V2_STORE_ONLY],
+            ],
+            "fa_migration_2_batch",
+        )
+        .await;
+    }
 
-    //     process_fa_txns(
-    //         &[IMPORTED_MAINNET_TXNS_1957950162_FA_MIGRATION_V2_STORE_ONLY],
-    //         Some("fa_migration_2_batch".to_string()),
-    //     )
-    //     .await;
-    // }
+    #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
+    async fn test_fungible_asset_processor_asset_type_null() {
+        sequential_multi_transaction_helper_function(
+            &[&[IMPORTED_MAINNET_TXNS_2308282694_ASSET_TYPE_V1_NULL], &[
+                IMPORTED_MAINNET_TXNS_2308283617_ASSET_TYPE_V1_NULL_2,
+            ]],
+            "asset_type_null",
+        )
+        .await;
+    }
 
-    // Helper function to abstract out the transaction processing
-    async fn process_fa_txns(txns: &[&[u8]], test_case_name: Option<String>) {
-        let (diff_flag, custom_output_path) = get_test_config();
+    /// Tests processing of two transactions sequentially
+    /// Validates handling of multiple transactions with shared context
+    async fn sequential_multi_transaction_helper_function(
+        txn_batches: &[&[&[u8]]],
+        output_name: &str,
+    ) {
+        let (generate_flag, custom_output_path) = get_test_config();
         let output_path = custom_output_path.unwrap_or_else(|| DEFAULT_OUTPUT_FOLDER.to_string());
 
-        let (db, mut test_context) = setup_test_environment(txns).await;
+        let mut db = PostgresTestDatabase::new();
+        db.setup().await.unwrap();
+
+        for (i, txn_batch) in txn_batches.iter().enumerate() {
+            let is_last = i == txn_batches.len() - 1;
+            process_transactions(
+                &mut db,
+                txn_batch,
+                output_name,
+                is_last && generate_flag,
+                &output_path,
+                is_last,
+            )
+            .await;
+        }
+    }
+
+    async fn process_transactions(
+        db: &mut PostgresTestDatabase,
+        txns: &[&[u8]],
+        transaction_name: &str,
+        generate_flag: bool,
+        output_path: &str,
+        should_validate: bool,
+    ) {
+        let mut test_context = SdkTestContext::new(txns);
+        if test_context.init_mock_grpc().await.is_err() {
+            panic!("Failed to initialize mock grpc");
+        };
 
         let db_url = db.get_db_url();
         let (indexer_processor_config, processor_name) =
@@ -290,28 +336,48 @@ mod sdk_fungible_asset_processor_tests {
             fungible_asset_processor,
             load_data,
             db_url,
-            diff_flag,
-            output_path.clone(),
-            test_case_name.clone(),
+            generate_flag,
+            output_path.to_string(),
+            Some(transaction_name.to_string()),
         )
         .await
         {
             Ok(mut db_value) => {
-                let _ = validate_json(
-                    &mut db_value,
-                    test_context.get_request_start_version(),
-                    processor_name,
-                    output_path.clone(),
-                    test_case_name,
-                );
+                if should_validate {
+                    let _ = validate_json(
+                        &mut db_value,
+                        test_context.get_request_start_version(),
+                        processor_name,
+                        output_path.to_string(),
+                        Some(transaction_name.to_string()),
+                    );
+                }
             },
             Err(e) => {
                 panic!(
-                    "Test failed on transactions {:?} due to processor error: {}",
-                    test_context.get_test_transaction_versions(),
-                    e
+                    "Test failed on {} due to processor error: {}",
+                    transaction_name, e
                 );
             },
         }
+    }
+
+    // Update the existing process_single_batch_txns function to use the new merged function
+    async fn process_single_batch_txns(txns: &[&[u8]], test_case_name: Option<String>) {
+        let (generate_flag, custom_output_path) = get_test_config();
+        let output_path = custom_output_path.unwrap_or_else(|| DEFAULT_OUTPUT_FOLDER.to_string());
+
+        let mut db = PostgresTestDatabase::new();
+        db.setup().await.unwrap();
+
+        process_transactions(
+            &mut db,
+            txns,
+            &test_case_name.unwrap_or_default(),
+            generate_flag,
+            &output_path,
+            true, // Assuming we want to validate for fungible asset transactions
+        )
+        .await;
     }
 }
